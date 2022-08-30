3a593212a1424bf1e2e41a515d4e77477dcb3064
iwannaberealnerd <iwannaberealnerd@gmail.com>
2022-08-30 16:18:42 +0900

    [Feat] next-transpile-modules 추가

--- apps/gocho_web/next.config.js
+++ apps/gocho_web/next.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const withTM = require("next-transpile-modules")(["component"]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -9,3 +12,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+// module.exports = withTM(nextConfig);
