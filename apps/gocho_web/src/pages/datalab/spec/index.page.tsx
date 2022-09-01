924e4b790ee1a78e3960e6a031cba7ca49676320
ViKing <112384040+DeepLeHR-Vi@users.noreply.github.com>
2022-09-01 16:55:38 +0900

    Revert "Feature/playwright"

--- apps/gocho_web/src/pages/datalab/spec/index.page.tsx
+++ apps/gocho_web/src/pages/datalab/spec/index.page.tsx
@@ -5,12 +5,8 @@ import axios from "axios";
 import { useUserInfo } from "@api/auth";
 import { useModal } from "@recoil/hook/modal";
 
-import { Layout } from "@component/layout";
-import { AsideMenu } from "./component/asideMenu";
 import { CarouselPart } from "./part/carouselPart";
 import { ListPart } from "./part/listPart";
-
-import { title, colorPoint, flexBox } from "./style";
 
 const MainList: NextPage = () => {
   const { setCurrentModal, closeModal } = useModal();
@@ -29,21 +25,12 @@ const MainList: NextPage = () => {
     return () => {
       closeModal();
     };
-  }, [closeModal]);
+  }, []);
 
   return (
     <main>
       <CarouselPart />
-      <Layout>
-        <h1 css={title}>
-          <span css={colorPoint}>All </span>
-          스펙 List 📃✨
-        </h1>
-        <div css={flexBox}>
-          <AsideMenu />
-          <ListPart />
-        </div>
-      </Layout>
+      <ListPart />
     </main>
   );
 };
--- apps/gocho_web/src/pages/datalab/spec/part/listPart/style.ts
+++ apps/gocho_web/src/pages/datalab/spec/part/listPart/style.ts
@@ -1,19 +1,68 @@
 import { css } from "@emotion/react";
-
 import { COLORS } from "@style/constant";
 import { PC_HOVER } from "@style/mediaQuery";
 
 export const partContainer = css`
   background-color: ${COLORS.GRAY100};
-  width: calc(100% - 6rem);
+  padding: 6rem 0;
+`;
+
+export const title = css`
+  font-size: 1.75rem;
+  font-weight: 700;
+`;
+
+export const colorPoint = css`
+  color: ${COLORS.BLUE_NEON30};
 `;
 
 export const mainContainer = css`
   display: flex;
   align-items: flex-start;
+  margin-top: 2rem;
+`;
+
+export const menu = css`
+  position: sticky;
+  top: 0;
+  left: 0;
+  padding-top: 120px;
+  z-index: 10;
+`;
+
+export const asideProfile = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 0.75rem;
+  width: 5rem;
+  background-color: ${COLORS.GRAY90};
+  border-radius: 1.5rem;
+  color: ${COLORS.GRAY40};
+  margin-bottom: 0.5rem;
+  padding: 1rem;
+  word-break: break-all;
+  text-align: center;
+`;
+
+export const loginNickname = css`
+  margin-top: 0.5rem;
+`;
+
+export const asideButton = css`
+  font-size: 0.75rem;
+  width: 5rem;
+  background-color: ${COLORS.GRAY90};
+  border-radius: 1.5rem;
+  color: ${COLORS.GRAY40};
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  word-break: break-all;
+  text-align: center;
 `;
 
 export const listContainer = css`
+  margin-left: 1rem;
   flex-grow: 1;
 `;
 
