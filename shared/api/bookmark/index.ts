6c173d5a36ec36c9b826eb6bfa99f99743bd862d
iwannaberealnerd@deeplehr.com <iwannaberealnerd@deeplehr.com>
2022-09-26 10:26:58 +0900

    [fix] 파일삭제

--- shared/api/bookmark/index.ts
+++ shared/api/bookmark/index.ts
@@ -1,9 +1,7 @@
 export { useUserJobBookmarkArr } from "./get/useUserJobBookmarkArr";
 export { useUserCompanyBookmarkArr } from "./get/useUserCompanyBookmarkArr";
 export { useDeleteJobBookmarkArr } from "./delete/useDeleteJobBookmarkArr";
-export { useAddUserBookmark } from "./add/useAddUserBookmark";
 export { useAddJobBookmarkArr } from "./add/useAddJobBookmarkArr";
-export { useDeleteUserBookmark } from "./delete/useDeleteBookmark";
 
 export { useAddCompanyBookmarkArr } from "./add/useAddCompanyBookmarkArr";
 export { useDeleteCompanyBookmarkArr } from "./delete/useDeleteCompanyBookmarkArr";
