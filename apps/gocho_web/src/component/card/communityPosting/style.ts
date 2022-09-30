b53d4491bec53496d912eb65f25088579ebaf99b
Mumu <hsm9300@deeplehr.com>
2022-09-30 14:23:29 +0900

    [Fix] shared-style/shorten -> shared-style/shorten으로 변경

--- apps/gocho_web/src/component/card/communityPosting/style.ts
+++ apps/gocho_web/src/component/card/communityPosting/style.ts
@@ -1,5 +1,5 @@
 import { css, SerializedStyles } from "@emotion/react";
-import { shorten } from "shared-style/shorten";
+import { shorten } from "shared-style/common";
 import { COLORS } from "shared-style/color";
 
 export const cardWrapper = css`
