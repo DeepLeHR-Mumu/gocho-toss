e985a825dc8e98cb17fa71935592dec30dbe856b
Vi <yuyukgh@deeplehr.com>
2022-10-18 13:50:47 +0900

    [Feat] jd list filter에 따른 스크롤 유지 []

--- apps/gocho_mobile/src/pages/jd/list/index.page.tsx
+++ apps/gocho_mobile/src/pages/jd/list/index.page.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
 
 import { MetaHead } from "shared-ui/common/atom/metaHead";
@@ -11,10 +11,30 @@ import { ListPart } from "./part/listPart";
 const JdList: NextPage = () => {
   const router = useRouter();
   const adPartRef = useRef<HTMLDivElement | null>(null);
+  const [currentScrollY, setCurrentScrollY] = useState(0);
+
+  const changeWindowScrollY = () => {
+    setCurrentScrollY(window.scrollY);
+  };
 
   useEffect(() => {
-    if (adPartRef.current) window.scrollTo(0, adPartRef.current?.offsetHeight);
-  }, [router]);
+    window.addEventListener("scroll", changeWindowScrollY);
+
+    return () => {
+      window.removeEventListener("scroll", changeWindowScrollY);
+    };
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, currentScrollY);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.query.order]);
+
+  useEffect(() => {
+    if (adPartRef.current && Number(router.query.page) !== 1) {
+      window.scrollTo(0, adPartRef?.current.offsetHeight);
+    }
+  }, [router.query.page]);
 
   return (
     <main>
---
+++ apps/gocho_web/src/pages/jd/list/style.ts
@@ -0,0 +1,5 @@
+import { css } from "@emotion/react";
+
+export const adPartContainer = css`
+  overflow: hidden;
+`;
