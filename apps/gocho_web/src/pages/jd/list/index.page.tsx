6faa37054c3ec0a53d937542d0c5a1b1947f2393
Vi <yuyukgh@deeplehr.com>
2022-10-19 14:29:17 +0900

    D/feat/jdList : 추가되었던 스크롤 이동 제거

--- apps/gocho_web/src/pages/jd/list/index.page.tsx
+++ apps/gocho_web/src/pages/jd/list/index.page.tsx
@@ -1,47 +1,15 @@
 import { NextPage } from "next";
-import { useEffect, useRef, useState } from "react";
-import { useRouter } from "next/router";
 
 import { MetaHead } from "shared-ui/common/atom/metaHead";
 import { META_JD_LIST } from "shared-constant/meta";
 import { AdPart } from "./part/adPart";
 import { ListPart } from "./part/listPart";
-import { adPartContainer } from "./style";
 
 const JobsList: NextPage = () => {
-  const router = useRouter();
-  const adPartRef = useRef<HTMLDivElement | null>(null);
-  const [currentScrollY, setCurrentScrollY] = useState(0);
-
-  const changeWindowScrollY = () => {
-    setCurrentScrollY(window.scrollY);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", changeWindowScrollY);
-
-    return () => {
-      window.removeEventListener("scroll", changeWindowScrollY);
-    };
-  }, []);
-
-  useEffect(() => {
-    window.scrollTo(0, currentScrollY);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [router.query.order]);
-
-  useEffect(() => {
-    if (adPartRef.current && Number(router.query.page) !== 1) {
-      window.scrollTo(0, adPartRef?.current.offsetHeight);
-    }
-  }, [router.query.page]);
-
   return (
     <main>
       <MetaHead metaData={META_JD_LIST} />
-      <div ref={adPartRef} css={adPartContainer}>
-        <AdPart />
-      </div>
+      <AdPart />
       <ListPart />
     </main>
   );
