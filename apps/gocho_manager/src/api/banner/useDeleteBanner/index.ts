6be729dbc0c5ca33dd067eb752e78d86ac79b07b
Mumu <hsm9300@deeplehr.com>
2022-10-20 19:47:29 +0900

    [Feat] 공고 상단 배너 업로드 기능 추가

---
+++ apps/gocho_manager/src/api/banner/useDeleteBanner/index.ts
@@ -0,0 +1,20 @@
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+
+import { AdminResponseDef } from "shared-type/api/responseType";
+import { axiosInstance } from "@api/axiosInstance";
+import { useDeleteBannerProps, DeleteBannerDef, RequestObjDef } from "./type";
+
+export const deleteBanner: DeleteBannerDef = async (requestObj) => {
+  const token =
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkluZm8iLCJpZCI6MSwiZW1haWwiOiJhZG1pbkBkZWVwbGVoci5jb20iLCJyb2xlIjoiUk9MRV9BRE1JTiIsImlhdCI6MTY2NjI1NTAxOCwiZXhwIjoxNjY3NDY0NjE4LCJpc3MiOiLqs6DstIjrjIDsobguY29tIn0.MCFcFa8nuSC_uYgu35U8-rmN5bG5IGHbA2CjXjMwM94sJ5GPwX6sHE49ObLzPb9jtrMAC4xLYdvSe5CIe_FP_g";
+  const { data } = await axiosInstance.delete(`/admin/banners/${requestObj.bannerId}`, {
+    headers: { "x-access-token": token },
+  });
+  return data;
+};
+
+export const useDeleteBanner: useDeleteBannerProps = () => {
+  const mutationResult = useMutation<AdminResponseDef, AxiosError, RequestObjDef>(deleteBanner);
+  return mutationResult;
+};
