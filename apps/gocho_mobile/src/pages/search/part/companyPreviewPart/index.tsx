87deb58f7f3274aebde86a170e4798c6fe512536
Vi <yuyukgh@deeplehr.com>
2022-10-26 15:01:49 +0900

    M/refactor/search

--- apps/gocho_mobile/src/pages/search/part/companyPreviewPart/index.tsx
+++ apps/gocho_mobile/src/pages/search/part/companyPreviewPart/index.tsx
@@ -1,28 +1,40 @@
 import { FunctionComponent } from "react";
+import { useRouter } from "next/router";
+import { BsChevronRight } from "react-icons/bs";
 
 import { useUserInfo } from "shared-api/auth";
 import { useUserCompanyBookmarkArr } from "shared-api/bookmark";
 import { dummyArrCreator } from "shared-util/dummyArrCreator";
 import { CompanyCard } from "shared-ui/card/companyCard";
+import { NormalButton } from "shared-ui/common/atom/button";
+import { COLORS } from "shared-style/color";
+import { useUnifiedCompanySearchArr } from "shared-api/company";
 
-import { listContainer, noDataText } from "./style";
-import { CompanyListPartProps } from "./type";
+import { COMPANY_PREVIEW_RESULT_LIMIT } from "@pages/search/constant";
+import { listContainer, noDataText, buttonBox } from "./style";
 
-export const CompanyPreviewPart: FunctionComponent<CompanyListPartProps> = ({ companyDataArr, count, isLoading }) => {
+export const CompanyPreviewPart: FunctionComponent = () => {
+  const router = useRouter();
+
   const { data: userData } = useUserInfo();
+  const { data: companyDataArr, isLoading: isCompanyDataArrLoading } = useUnifiedCompanySearchArr({
+    searchWord: router.query.q,
+    page: router.query.page,
+    limit: COMPANY_PREVIEW_RESULT_LIMIT,
+  });
   const { data: userCompanyBookmarkArr, refetch } = useUserCompanyBookmarkArr({ userId: userData?.id });
 
-  if (!companyDataArr || isLoading) {
+  if (!companyDataArr || isCompanyDataArrLoading) {
     return (
       <div css={listContainer}>
-        {dummyArrCreator(2).map((dummy) => {
+        {dummyArrCreator(COMPANY_PREVIEW_RESULT_LIMIT).map((dummy) => {
           return <CompanyCard isSkeleton key={`UnifiedSearchCompanyCardSkeleton${dummy}`} />;
         })}
       </div>
     );
   }
 
-  if (count === 0) {
+  if (companyDataArr.count === 0) {
     return (
       <div css={listContainer}>
         <p css={noDataText}>검색 결과가 없습니다.</p>
@@ -32,7 +44,7 @@ export const CompanyPreviewPart: FunctionComponent<CompanyListPartProps> = ({ co
 
   return (
     <section css={listContainer}>
-      {companyDataArr.slice(0, 2).map((companyData) => {
+      {companyDataArr.companyDataArr.map((companyData) => {
         const isBookmarked = Boolean(
           userCompanyBookmarkArr?.some((company) => {
             return company.id === companyData.id;
@@ -48,6 +60,28 @@ export const CompanyPreviewPart: FunctionComponent<CompanyListPartProps> = ({ co
           />
         );
       })}
+
+      {companyDataArr.count !== 0 && (
+        <div css={buttonBox}>
+          <NormalButton
+            text="기업정보 더보기"
+            iconObj={{
+              icon: BsChevronRight,
+              color: COLORS.BLUE_FIRST40,
+              size: 0.75,
+              position: "right",
+            }}
+            variant="outlined"
+            buttonClick={() => {
+              router.push({
+                pathname: "/search",
+                query: { q: router.query.q, page: 1, menu: "기업" },
+              });
+            }}
+            wide={false}
+          />
+        </div>
+      )}
     </section>
   );
 };
--- apps/gocho_mobile/src/pages/search/part/jobPreviewPart/index.tsx
+++ apps/gocho_mobile/src/pages/search/part/jobPreviewPart/index.tsx
@@ -1,28 +1,41 @@
 import { FunctionComponent } from "react";
+import { useRouter } from "next/router";
+import { BsChevronRight } from "react-icons/bs";
 
 import { dummyArrCreator } from "shared-util/dummyArrCreator";
+import { useUnifiedJobSearchArr } from "shared-api/job";
+import { useUserInfo } from "shared-api/auth";
+import { NormalButton } from "shared-ui/common/atom/button";
+import { useUserJobBookmarkArr } from "shared-api/bookmark";
+import { COLORS } from "shared-style/color";
+
 import { JobCard } from "@component/common/molecule/jobCard";
 
-import { useUserInfo } from "shared-api/auth";
-import { useUserJobBookmarkArr } from "shared-api/bookmark";
-import { listContainer, noDataText } from "./style";
-import { JobPreviewPartProps } from "./type";
+import { JOB_PREVIEW_RESULT_LIMIT } from "@pages/search/constant";
+import { listContainer, noDataText, buttonBox } from "./style";
 
-export const JobPreviewPart: FunctionComponent<JobPreviewPartProps> = ({ jobDataArr, count, isLoading }) => {
+export const JobPreviewPart: FunctionComponent = () => {
+  const router = useRouter();
+
   const { data: userData } = useUserInfo();
+  const { data: jobDataArr, isLoading: isJobLoading } = useUnifiedJobSearchArr({
+    searchWord: router.query.q,
+    page: router.query.page,
+    limit: JOB_PREVIEW_RESULT_LIMIT,
+  });
   const { data: userJobBookmarkArr } = useUserJobBookmarkArr({ userId: userData?.id });
 
-  if (!jobDataArr || isLoading) {
+  if (!jobDataArr || isJobLoading) {
     return (
       <div css={listContainer}>
-        {dummyArrCreator(4).map((dummy) => {
+        {dummyArrCreator(JOB_PREVIEW_RESULT_LIMIT).map((dummy) => {
           return <JobCard isSkeleton key={`UnifiedSearchJobPreviewSkeleton${dummy}`} />;
         })}
       </div>
     );
   }
 
-  if (count === 0) {
+  if (jobDataArr.count === 0) {
     return (
       <div css={listContainer}>
         <p css={noDataText}>검색 결과가 없습니다.</p>
@@ -32,7 +45,7 @@ export const JobPreviewPart: FunctionComponent<JobPreviewPartProps> = ({ jobData
 
   return (
     <section css={listContainer}>
-      {jobDataArr.slice(0, 4).map((jobData) => {
+      {jobDataArr.jobDataArr.map((jobData) => {
         const isBookmarked = Boolean(
           userJobBookmarkArr?.some((job) => {
             return job.id === jobData.id;
@@ -47,6 +60,28 @@ export const JobPreviewPart: FunctionComponent<JobPreviewPartProps> = ({ jobData
           />
         );
       })}
+
+      {jobDataArr?.count !== 0 && (
+        <div css={buttonBox}>
+          <NormalButton
+            text="채용공고 더보기"
+            iconObj={{
+              icon: BsChevronRight,
+              color: COLORS.BLUE_FIRST40,
+              size: 0.75,
+              position: "right",
+            }}
+            variant="outlined"
+            buttonClick={() => {
+              router.push({
+                pathname: "/search",
+                query: { q: router.query.q, page: 1, menu: "공고" },
+              });
+            }}
+            wide={false}
+          />
+        </div>
+      )}
     </section>
   );
 };
