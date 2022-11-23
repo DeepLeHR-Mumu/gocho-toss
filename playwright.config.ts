a47ba65de5fbd847bca0c2ec87c3a98e77549f39
Vi <yuyukgh@deeplehr.com>
2022-11-23 18:05:08 +0900

    playwright 병렬 false

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ const config: PlaywrightTestConfig = {
   expect: {
     timeout: 6000,
   },
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
