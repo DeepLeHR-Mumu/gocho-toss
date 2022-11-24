aa78d20ef0444227e53b0c9edf3a8f1db2447e8a
ahri <iwannaberealnerd@deeplehr.com>
2022-11-25 01:18:41 +0900

    [fix] 스크립트 명령어에 -w 추가

--- .github/bash/playwright_check.sh
+++ .github/bash/playwright_check.sh
@@ -2,10 +2,10 @@
 
 if pnpm list | grep "turbo"
     then :
-    else pnpm add turbo && pnpm list | grep "turbo";
+    else pnpm add -w turbo && pnpm list | grep "turbo";
 fi
 
 if pnpm list | grep "playwright";
     then pnpm turbo $1;
-    else pnpm add @playwright/test@1.28.0 && pnpm turbo $1;
+    else pnpm add -w @playwright/test@1.28.0 && pnpm turbo $1;
 fi
