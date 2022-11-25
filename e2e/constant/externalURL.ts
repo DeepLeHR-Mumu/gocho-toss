a3ea368b707b4114e9691834c91da64c798476d2
Vi <yuyukgh@deeplehr.com>
2022-11-25 17:31:32 +0900

    [Fix] 테스트코드 개발서버 재변경

--- e2e/constant/externalURL.ts
+++ e2e/constant/externalURL.ts
@@ -1,7 +1,3 @@
-export const BACKEND_URL = `${
-  process.env.NODE_ENV === "development" ? "https://dev.gocho-back.com/v1" : "https://gocho-back.com/v1"
-}`;
-export const MANAGER_BACKEND_URL = `${
-  process.env.NODE_ENV === "development" ? "https://dev.manager.gocho-back.com" : "https://manager.gocho-back.com"
-}`;
+export const BACKEND_URL = "https://dev.gocho-back.com/v1";
+export const MANAGER_BACKEND_URL = "https://manager.gocho-back.com";
 export const CDN_URL = "http://cdn.gocho-back.com/";
