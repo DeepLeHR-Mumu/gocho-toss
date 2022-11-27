aa4df14fcf7636c82cb7a9c62a059f4f82b2f56c
Vi <yuyukgh@deeplehr.com>
2022-11-27 20:29:16 +0900

    [fix] 백엔드 서버 production, preview, dev 구분제공 변경

--- shared/constant/externalURL.ts
+++ shared/constant/externalURL.ts
@@ -1,8 +1,9 @@
 export const BACKEND_URL = `${
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "https://gocho-back.com/v1" : "https://dev.gocho-back.com/v1"
 }`;
-// export const BACKEND_URL = process.env.API_URL || "https://dev.gocho-back.com/v1";
 export const MANAGER_BACKEND_URL = `${
-  process.env.NODE_ENV === "development" ? "https://dev.manager.gocho-back.com" : "https://manager.gocho-back.com"
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+    ? "https://manager.gocho-back.com"
+    : "https://dev.manager.gocho-back.com"
 }`;
 export const CDN_URL = "http://cdn.gocho-back.com/";
