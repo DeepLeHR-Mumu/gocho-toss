a2188bc3ba8789e82612f2e58eb4c1423298038b
ahri <iwannaberealnerd@deeplehr.com>
2022-11-29 15:12:31 +0900

    [test] 테스트 개편

--- apps/gocho_mobile/playwright.config.ts
+++ apps/gocho_mobile/playwright.config.ts
@@ -8,34 +8,13 @@ const config: PlaywrightTestConfig = {
   },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
   reporter: process.env.CI ? "github" : "list",
 
   projects: [
-    // gocho_web
-    {
-      name: "chromium",
-      testDir: "./e2e/gocho_web",
-      use: {
-        baseURL: process.env.GOCHO_WEB_URL || "http://localhost:3000",
-        ...devices["Desktop Chrome"],
-      },
-    },
-
-    // {
-    //   name: "webkit",
-    //   testDir: "./e2e/gocho_web",
-    //   use: {
-    //     baseURL: process.env.GOCHO_WEB_URL || "http://localhost:3000",
-    //     ...devices["Desktop Safari"],
-    //   },
-    // },
-
     /* gocho_mobile */
     {
       name: "Mobile Chrome",
-      testDir: "./e2e/gocho_mobile",
+      testDir: "./e2e",
       use: {
         baseURL: process.env.GOCHO_MOBILE_URL || "http://localhost:3000",
         ...devices["Galaxy S9+"],
