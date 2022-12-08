936ce3fffdc03ba0e367862ac6d1954362b65fcb
Vi <yuyukgh@deeplehr.com>
2022-12-08 13:48:40 +0900

    [Test] sitemap 배포환경 테스트

--- apps/gocho_web/src/pages/server-sitemap.xml/index.tsx
+++ apps/gocho_web/src/pages/server-sitemap.xml/index.tsx
@@ -1,24 +1,51 @@
 import { getServerSideSitemap } from "next-sitemap";
 import { GetServerSideProps } from "next";
+import { QueryClient } from "@tanstack/react-query";
 
-const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000";
+import { GOCHO_DESKTOP_URL } from "shared-constant/internalURL";
+import { jobArrKeyObj } from "shared-constant/queryKeyFactory/job/jobArrKeyObj";
+import { getJobArr } from "shared-api/job/useJobArr";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const fields = [
-    {
-      loc: baseUrl,
-      lastmod: new Date().toISOString(),
-      // changefreq
-      // priority
-    },
-    {
-      loc: `${baseUrl}/dynamic-path-2`,
-      lastmod: new Date().toISOString(),
-      // changefreq
-      // priority
-    },
-  ];
-  return getServerSideSitemap(ctx, fields);
+  const queryClient = new QueryClient();
+  const lastmod = new Date().toISOString();
+
+  const jdArrData = await queryClient.fetchQuery(
+    jobArrKeyObj.jobArr({ limit: 0, order: "recent", filter: "valid" }),
+    getJobArr
+  );
+
+  const jdField = jdArrData.data.map((jd) => {
+    return {
+      loc: `${GOCHO_DESKTOP_URL}/jd/detail/${jd.id}`,
+      //   changefreq: "daily",
+      //   priority: "0.9",
+      lastmod,
+    };
+  });
+
+  //   const jdField = [
+  //     {
+  //       loc: `${GOCHO_DESKTOP_URL}/jd/detail`,
+  //       lastmod,
+  //     },
+  //   ];
+  //   const companyDetailField = [
+  //     {
+  //       loc: `${GOCHO_DESKTOP_URL}/company/??/detail`,
+  //       lastmod,
+  //     },
+  //   ];
+  //   const companyJdField = [
+  //     {
+  //       loc: `${GOCHO_DESKTOP_URL}/company/??/jd`,
+  //       lastmod,
+  //     },
+  //   ];
+
+  const field = [...jdField];
+
+  return getServerSideSitemap(ctx, field);
 };
 
 export default () => {
