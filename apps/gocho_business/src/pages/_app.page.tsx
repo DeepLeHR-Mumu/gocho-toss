bde5c19f0b3afd423d61585de0734a2cf281c58b
ahri <iwannaberealnerd@deeplehr.com>
2022-12-11 16:47:41 +0900

    [feat] rq, axios 및 rq 초기설정

--- apps/gocho_business/src/pages/_app.page.tsx
+++ apps/gocho_business/src/pages/_app.page.tsx
@@ -1,8 +1,45 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import { Global } from "@emotion/react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider, Hydrate } from "@tanstack/react-query";
+import axios from "axios";
+
+import { globalStyle } from "style/globalStyle";
+import { useRouter } from "next/router";
 
 function BusinessService({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const [queryClient] = useState(() => {
+    return new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 15000,
+          refetchOnWindowFocus: false,
+          keepPreviousData: true,
+          retry: 0,
+          onError: (error) => {
+            if (axios.isAxiosError(error) && error.response?.status === 404) {
+              router.push("/404");
+            }
+            if (axios.isAxiosError(error) && error.response?.status === 500) {
+              router.push("/500");
+            }
+          },
+        },
+      },
+    });
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Global styles={globalStyle} />
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
-export default BusinessService
+export default BusinessService;
