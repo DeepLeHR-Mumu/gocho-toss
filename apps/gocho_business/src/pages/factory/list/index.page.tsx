eb74acec4302d8cf8ef0bb5f66aae85eadd67c65
ahri <iwannaberealnerd@deeplehr.com>
2022-12-15 13:14:29 +0900

    [feat] factory card flex 적용

--- apps/gocho_business/src/pages/factory/list/index.page.tsx
+++ apps/gocho_business/src/pages/factory/list/index.page.tsx
@@ -2,14 +2,26 @@ import type { NextPage } from "next";
 
 import { Layout } from "@/components/global/layout";
 import { CompanyInfoPart } from "./part/companyInfoPart";
+import { RegisterPart } from "./part/registerPart";
+import { FactoryCardPart } from "./part/factoryCardPart";
+import { cardContainer, factoryPartContainer } from "./style";
 
 const FactoryListPage: NextPage = () => (
-  <>
+  <main>
     <CompanyInfoPart />
     <Layout>
-      <div>Factory List Page</div>
+      <RegisterPart />
+      <h2>공장 목록</h2>
+      <div css={factoryPartContainer}>
+        <section css={cardContainer}>
+          <FactoryCardPart name="weoifjaop;iwefj;oailwjef;oilawhnefgo;iawhne;ogihjawoi;egja;owiejg;oiawjego;iawjeiopgjqaw/;leogjawipoegjioaw" />
+          <FactoryCardPart name="weoifjaop;iwefj;oailwjef;oilawhnefgo;iawhne;ogihjawoi;egja;owiejg;oiawjego;iawjeiopgjqaw/;leogjawipoegjioawleogjawipoegjioawleogjawipoegjioawleogjawipoegjioawleogjawipoegjioawleogjawipoegjioawleogjawipoegjioawleogjawipoegjioawleogjawipoegjioawleogjawipoegjioawleogjawipoegjioaw" />
+          <FactoryCardPart name="woiejfoiawqjeiofajweiof" />
+          <FactoryCardPart name="823fyp98q3h4fgi;ohqaw;oiergjha;iowejg;oaiwjegioaewj;oigjawei;ogjaio;w" />
+        </section>
+      </div>
     </Layout>
-  </>
+  </main>
 );
 
 export default FactoryListPage;
---
+++ apps/gocho_business/src/pages/factory/list/style.ts
@@ -0,0 +1,14 @@
+import { css } from "@emotion/react";
+
+export const factoryPartContainer = css`
+  display: flex;
+  justify-content: center;
+`;
+
+export const cardContainer = css`
+  display: flex;
+  flex-wrap: wrap;
+  width: 62rem;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
