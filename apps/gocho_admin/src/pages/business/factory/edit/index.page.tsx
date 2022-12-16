f384179558c9c53f4cd55c7e363d15386c6a37f8
Mumu <hsm9300@deeplehr.com>
2022-12-16 14:27:59 +0900

    [Feat] 공장 요청 상세 페이지에 승인 및 반려 버튼 추가

--- apps/gocho_admin/src/pages/business/factory/edit/index.page.tsx
+++ apps/gocho_admin/src/pages/business/factory/edit/index.page.tsx
@@ -1,12 +1,19 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useQueryClient } from "@tanstack/react-query";
 
 import { useFactoryDetail } from "@api/factory/useFactoryDetail";
 import { useEditFactoryRequest } from "@api/factory/useEditFactoryRequest";
+import { useAcceptFactory } from "@api/factory/useAcceptFactory";
+import { useRejectFactory } from "@api/factory/useRejectFactory";
+import { factoryArrKeyObj } from "@api/factory/useFactoryArr/type";
 import { mainContainer, pageTitle } from "@style/commonStyles";
 import { ErrorScreen, LoadingScreen } from "@component/screen";
 
+import { acceptButton, buttonContainer, rejectButton } from "./style";
+
 const FactoryEditDetail: NextPage = () => {
+  const queryClient = useQueryClient();
   const router = useRouter();
   const factoryId = Number(router.query.id);
 
@@ -22,6 +29,31 @@ const FactoryEditDetail: NextPage = () => {
     isError: isAfterError,
   } = useEditFactoryRequest({ factoryId });
 
+  const { mutate: acceptFactoryMutate } = useAcceptFactory();
+  const { mutate: rejectFactoryMutate } = useRejectFactory();
+
+  const acceptFactoryHandler = () => {
+    acceptFactoryMutate(
+      { factoryId, type: "update" },
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries(factoryArrKeyObj.all);
+        },
+      }
+    );
+  };
+
+  const rejectFactoryHandler = () => {
+    rejectFactoryMutate(
+      { factoryId, type: "update" },
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries(factoryArrKeyObj.all);
+        },
+      }
+    );
+  };
+
   if (!factoryBeforeData || !factoryAfterData || isBeforeLoading || isAfterLoading) {
     return <LoadingScreen />;
   }
@@ -33,6 +65,26 @@ const FactoryEditDetail: NextPage = () => {
   return (
     <main css={mainContainer}>
       <h2 css={pageTitle}>공장 변경 요청 확인</h2>
+      <div css={buttonContainer}>
+        <button
+          type="submit"
+          css={acceptButton}
+          onClick={() => {
+            return acceptFactoryHandler;
+          }}
+        >
+          수정 승인
+        </button>
+        <button
+          type="submit"
+          css={rejectButton}
+          onClick={() => {
+            return rejectFactoryHandler;
+          }}
+        >
+          수정 반려
+        </button>
+      </div>
     </main>
   );
 };
--- apps/gocho_admin/src/pages/business/factory/register/index.page.tsx
+++ apps/gocho_admin/src/pages/business/factory/register/index.page.tsx
@@ -1,15 +1,46 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useQueryClient } from "@tanstack/react-query";
 
 import { useFactoryDetail } from "@api/factory/useFactoryDetail";
+import { useAcceptFactory } from "@api/factory/useAcceptFactory";
+import { useRejectFactory } from "@api/factory/useRejectFactory";
+import { factoryArrKeyObj } from "@api/factory/useFactoryArr/type";
 import { mainContainer, pageTitle } from "@style/commonStyles";
 import { ErrorScreen, LoadingScreen } from "@component/screen";
 
+import { acceptButton, buttonContainer, rejectButton } from "./style";
+
 const FactoryRegisterDetail: NextPage = () => {
+  const queryClient = useQueryClient();
   const router = useRouter();
   const factoryId = Number(router.query.id);
 
   const { data: factoryDataObj, isLoading, isError } = useFactoryDetail({ factoryId });
+  const { mutate: acceptFactoryMutate } = useAcceptFactory();
+  const { mutate: rejectFactoryMutate } = useRejectFactory();
+
+  const acceptFactoryHandler = () => {
+    acceptFactoryMutate(
+      { factoryId, type: "upload" },
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries(factoryArrKeyObj.all);
+        },
+      }
+    );
+  };
+
+  const rejectFactoryHandler = () => {
+    rejectFactoryMutate(
+      { factoryId, type: "upload" },
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries(factoryArrKeyObj.all);
+        },
+      }
+    );
+  };
 
   if (!factoryDataObj || isLoading) {
     return <LoadingScreen />;
@@ -22,6 +53,26 @@ const FactoryRegisterDetail: NextPage = () => {
   return (
     <main css={mainContainer}>
       <h2 css={pageTitle}>공장 등록 요청 확인</h2>
+      <div css={buttonContainer}>
+        <button
+          type="submit"
+          css={acceptButton}
+          onClick={() => {
+            return acceptFactoryHandler;
+          }}
+        >
+          등록 승인
+        </button>
+        <button
+          type="submit"
+          css={rejectButton}
+          onClick={() => {
+            return rejectFactoryHandler;
+          }}
+        >
+          등록 반려
+        </button>
+      </div>
     </main>
   );
 };
