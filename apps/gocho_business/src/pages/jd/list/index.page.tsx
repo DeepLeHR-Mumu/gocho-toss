28183206bc656eb4607601753cec3329c4981b34
Mumu <hsm9300@deeplehr.com>
2022-12-19 22:11:28 +0900

    [Feat] 각 페이지에 PageLayout 및 GlobalLayout 추가

--- apps/gocho_business/src/pages/jd/list/index.page.tsx
+++ apps/gocho_business/src/pages/jd/list/index.page.tsx
@@ -1,5 +1,21 @@
-import type { NextPage } from "next";
+import { ReactElement } from "react";
 
-const JdListPage: NextPage = () => <div>Jd List Page</div>;
+import type { NextPageWithLayout } from "@/pages/_app.page";
+import { PageLayout, GlobalLayout } from "@/components/global/layout";
+import { CompanyInfoPart } from "@/components/global/companyInfoPart";
+
+const JdListPage: NextPageWithLayout = () => (
+  <main>
+    <CompanyInfoPart />
+    <PageLayout>
+      <section>
+        This is JdListPage!!
+        <div>lol</div>
+      </section>
+    </PageLayout>
+  </main>
+);
+
+JdListPage.getLayout = (page: ReactElement) => <GlobalLayout>{page}</GlobalLayout>;
 
 export default JdListPage;
