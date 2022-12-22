830db392bd9198b3c792db298b043e827bf3387f
Mumu <hsm9300@deeplehr.com>
2022-12-22 23:20:07 +0900

    [Fix] 공고 리스트 API에 token 불러오기 추가

--- apps/gocho_business/src/apis/jd/useJdArr/index.ts
+++ apps/gocho_business/src/apis/jd/useJdArr/index.ts
@@ -8,12 +8,8 @@ import { GetJdArrDef, jdArrKeyObj, RequestObjDef, ResponseObjDef } from "./type"
 import { jdArrSelector } from "./util";
 
 export const getJdArr: GetJdArrDef = async () => {
-  const { data } = await axiosInstance.get("/jds", {
-    headers: {
-      "x-access-token":
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYW5hZ2VySW5mbyIsImlkIjowLCJjb21wYW55X2lkIjo5NjgsImNvbXBhbnlfbmFtZSI6IuqzoOy0iOuMgOyhuOuLt-y7tCIsImNvbXBhbnlfbG9nbyI6Imh0dHBzOi8vY2RuLmdvY2hvLWJhY2suY29tL2NvbXBhbmllcy85NjgvbG9nby5wbmciLCJlbWFpbCI6InRlZW1vQGRlZXBsZWhyLmNvbSIsIm5hbWUiOiLti7DrqqgiLCJkZXBhcnRtZW50Ijoi6rCc67Cc7YyAIiwicm9sZSI6IlJPTEVfTUFOQUdFUiIsInR5cGUiOjIsImlhdCI6MTY3MTYwODA0MywiZXhwIjoxNjcyODE3NjQzLCJpc3MiOiLqs6DstIjrjIDsobguY29tIn0.lQWFIMx68-bvxdShHN0exZaeE2dBUTztLIWYLA0GyKOJyYNJ1wcZr9ZRrHE_5tqPqOIyT0HLZhn5y92t_XBeQQ",
-    },
-  });
+  const token = localStorage.getItem("accessToken") as string;
+  const { data } = await axiosInstance.get("/jds", { headers: { "x-access-token": token } });
   return data;
 };
 
