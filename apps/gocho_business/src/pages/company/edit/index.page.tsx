e1f24357cb161ac64067bd6084e657f1b0543c07
Vi <yuyukgh@deeplehr.com>
2022-12-23 13:54:07 +0900

    [Feat] 기업 수정 작업중 공통 컴포넌트 작업을 위한 임시커밋

--- apps/gocho_business/src/pages/company/edit/index.page.tsx
+++ apps/gocho_business/src/pages/company/edit/index.page.tsx
@@ -1,10 +1,64 @@
 import { ReactElement } from "react";
+import { FiEdit, FiUsers } from "react-icons/fi";
 
 import { CompanyInfoPart } from "@/components/global/companyInfoPart";
-import { GlobalLayout } from "@/components/global/layout";
+import { CommonStatusChip } from "@/components/common";
+import { PageLayout, GlobalLayout } from "@/components/global/layout";
 import { NextPageWithLayout } from "@/pages/_app.page";
 
-const CompanyEditPage: NextPageWithLayout = () => <div>Company Edit Page</div>;
+import { cssObj } from "../style";
+
+const CompanyEditPage: NextPageWithLayout = () => (
+  <main css={cssObj.wrapper}>
+    <PageLayout>
+      <form css={cssObj.container}>
+        <header css={cssObj.header}>
+          <div>
+            <h2 css={cssObj.title}>기업정보</h2>
+            <p css={cssObj.desc}>기업 정보에 변화가 있다면 작성 후 수정완료를 눌러주세요</p>
+          </div>
+          <div css={cssObj.flexBox}>
+            <CommonStatusChip status="승인됨" />
+            <button type="submit">
+              <FiEdit /> 기업 정보 수정완료
+            </button>
+          </div>
+        </header>
+        <section css={cssObj.companyInfoBox}>
+          <div css={cssObj.lineBox}>
+            <strong css={cssObj.subTitle}>기업 형태</strong>
+            <p css={cssObj.textValue}>대기업</p>
+          </div>
+          <div css={cssObj.flexNormalBox}>
+            <div css={cssObj.lineBox}>
+              <strong css={cssObj.subTitle}>설립일</strong>
+              <p css={cssObj.textValue}>2022년 10월 22일</p>
+            </div>
+            <div css={cssObj.lineBox}>
+              <strong css={cssObj.subTitle}>사업자 번호</strong>
+              <p css={cssObj.textValue}>000-00-00000</p>
+            </div>
+          </div>
+          <div css={cssObj.flexNormalBox}>
+            <div css={cssObj.lineBox}>
+              <strong css={cssObj.subTitle}>사원수</strong>
+              <label htmlFor="employee_number">
+                <FiUsers />
+                <input
+                  type="number"
+                  onWheel={(event) => {
+                    event.currentTarget.blur();
+                  }}
+                />
+                명
+              </label>
+            </div>
+          </div>
+        </section>
+      </form>
+    </PageLayout>
+  </main>
+);
 
 CompanyEditPage.getLayout = (page: ReactElement) => (
   <GlobalLayout>
