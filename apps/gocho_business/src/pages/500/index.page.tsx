3f65510707a61366535bcf3409059bdb63c092e4
ahri <iwannaberealnerd@deeplehr.com>
2022-12-30 22:59:19 +0900

    [feat] 404, 500 페이지 완료

--- apps/gocho_business/src/pages/500/index.page.tsx
+++ apps/gocho_business/src/pages/500/index.page.tsx
@@ -1,5 +1,31 @@
-import { NextPage } from "next";
+import { ReactElement } from "react";
+import Image from "next/image";
+import { InvisibleH2 } from "shared-ui/common/atom/invisibleH2";
 
-const UnknownErrorPage: NextPage = () => <>500 Error page</>;
+import jobi_500 from "@/public/image/jobi_500.svg";
 
-export default UnknownErrorPage;
+import { NextPageWithLayout } from "@/pages/_app.page";
+import { Footer, GlobalLayout } from "@/components/global/layout";
+
+import { cssObj } from "./style";
+
+const NotFoundPage: NextPageWithLayout = () => (
+  <main css={cssObj.container}>
+    <InvisibleH2 title="없는 페이지" />
+    <div css={cssObj.jobiImage}>
+      <Image src={jobi_500} alt="" objectFit="fill" layout="fill" />
+    </div>
+    <p css={cssObj.title}>열심히 고치고 있습니다</p>
+    <div css={cssObj.catchPhraseContainer}>
+      <p css={cssObj.catchPhrase}>고장, 불편 사항은 아래 채널톡에서 신고 부탁드려요 😢</p>
+    </div>
+  </main>
+);
+
+NotFoundPage.getLayout = (page: ReactElement) => (
+  <GlobalLayout>
+    {page}
+    <Footer />
+  </GlobalLayout>
+);
+export default NotFoundPage;
