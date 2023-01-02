da6ea60a621e5483518af8d7f59fab2bcc32de19
Vi <yuyukgh@deeplehr.com>
2023-01-02 21:12:46 +0900

    [Feat] 기업정보 수정 테스트코드 업데이트

--- apps/gocho_business/e2e/company/edit/companyEdit.spec.ts
+++ apps/gocho_business/e2e/company/edit/companyEdit.spec.ts
@@ -1,29 +1,54 @@
 import { expect, test } from "@playwright/test";
 
+import { BUSINESS_BACKEND_URL } from "shared-constant/externalURL";
 import { INTERNAL_URL } from "@/constants/url";
 
 test.beforeEach(async ({ page }) => {
   await page.goto(INTERNAL_URL.LOGIN);
-  await page.getByPlaceholder("아이디(이메일)").fill("ahri@deeplehr.com");
+  await page.getByPlaceholder("아이디(이메일)").fill("vi@deeplehr.com");
   await page.getByPlaceholder("비밀번호").fill("deeple1!");
   await page.getByRole("button", { name: "로그인" }).click();
   await page.waitForLoadState("networkidle");
 });
 
-test("공장 정보 등록 및 삭제 테스트", async ({ page }) => {
-  await page.getByRole("link", { name: "공장" }).click();
-  await expect(page.getByText("고초대졸.business")).toBeVisible();
-  await expect(page.getByRole("heading", { name: "공장 등록" })).toHaveText("공장 등록");
-  await expect(page.getByRole("heading", { name: "공장 목록" })).toHaveText("공장 목록");
+test("기업 정보 get 및 expect 테스트", async ({ page, request }) => {
+  const myCompanyId = 968;
+  const [companyResponse] = await Promise.all([
+    page.waitForResponse((response) => response.url().includes("companies") && response.status() === 200),
+    page.waitForNavigation(),
+    await page.getByRole("link", { name: "기업 정보 수정" }).click(),
+  ]);
+  const companyData = await companyResponse.json();
 
-  await page.getByRole("link", { name: "공장" }).click();
-  const beforeFactoryListDataObj = await (
-    await page.waitForResponse((response) => response.url().includes("factories") && response.status() === 200)
-  ).json();
-  await page.waitForLoadState("networkidle");
-  await page.waitForTimeout(400);
+  const companyRequestResponse = await request.get(`${BUSINESS_BACKEND_URL}/companies/${myCompanyId}`);
+  const companyRequestData = await companyRequestResponse.json();
 
-  await page.locator("input[name='factory_name']").type("테스트 공장 1");
+  expect(companyData.data.size).toEqual(companyRequestData.data.size);
+  expect(companyData.data.employee_number).toEqual(companyRequestData.data.employee_number);
+  expect(companyData.data.found_date).toEqual(companyRequestData.data.found_date);
+  expect(companyData.data.business_number).toEqual(companyRequestData.data.business_number);
+  expect(companyData.data.address).toEqual(companyRequestData.data.address);
+  expect(companyData.data.intro).toEqual(companyRequestData.data.intro);
+  expect(companyData.data.pay_avg).toEqual(companyRequestData.data.pay_avg);
+  expect(companyData.data.pay_start).toEqual(companyRequestData.data.pay_start);
+  expect(companyData.data.pay_desc).toEqual(companyRequestData.data.pay_desc);
+  expect(companyData.data.nozo.exists).toEqual(companyRequestData.data.nozo.exists);
+  expect(companyData.data.nozo.desc).toEqual(companyRequestData.data.nozo.desc);
+  expect(companyData.data.welfare.money).toEqual(companyRequestData.data.welfare.money);
+  expect(companyData.data.welfare.health).toEqual(companyRequestData.data.welfare.health);
+  expect(companyData.data.welfare.life).toEqual(companyRequestData.data.welfare.life);
+  expect(companyData.data.welfare.holiday).toEqual(companyRequestData.data.welfare.holiday);
+  expect(companyData.data.welfare.facility).toEqual(companyRequestData.data.welfare.facility);
+  expect(companyData.data.welfare.vacation).toEqual(companyRequestData.data.welfare.vacation);
+  expect(companyData.data.welfare.growth).toEqual(companyRequestData.data.welfare.growth);
+  expect(companyData.data.welfare.etc).toEqual(companyRequestData.data.welfare.etc);
+});
+
+test("기업 정보 수정 테스트", async ({ page }) => {
+  await page.getByRole("link", { name: "기업 정보 수정" }).click();
+  await page.locator('input[name="employee_number"]').fill("55");
+  await page.locator('input[name="intro"]').fill("가짜 회사소개입니다");
+
   const popupPromise = page.waitForEvent("popup");
 
   await page.getByRole("button", { name: "주소찾기" }).click();
@@ -41,67 +66,21 @@ test("공장 정보 등록 및 삭제 테스트", async ({ page }) => {
   await popup.keyboard.press("Enter");
   await page.waitForTimeout(500);
 
-  await page.locator("input[name='product']").fill("생산품 입니다 생산품이요 생산품");
-  await page.locator("input[name='male_number']").fill("33");
-  await page.locator("input[name='female_number']").fill("55");
+  await page.locator('input[name="pay_start"]').fill("1000");
+  await page.locator('input[name="pay_avg"]').fill("1000");
+  await page.locator('strong:has-text("급여") ~ div > input').fill("급여 복지를 추가하자");
+  await page.locator('strong:has-text("급여") ~ div > input ~ button').click();
+  await page.locator('strong:has-text("의료") ~ div > input').fill("의료 복지를 추가하자");
+  await page.locator('strong:has-text("의료") ~ div > input ~ button').click();
+  await page.locator('strong:has-text("생활") ~ div > input').fill("생활 복지를 추가하자");
+  await page.locator('strong:has-text("생활") ~ div > input ~ button').click();
+  await page.locator('button[aria-label="의료 복지를 추가하자 제거하기"]').click();
 
-  await page.getByText("있음").first().click();
-  await page.getByText("있음").nth(1).click();
-  await page.waitForTimeout(500);
+  const [companyRequest] = await Promise.all([
+    page.waitForRequest((putRequest) => putRequest.url().includes("companies")),
+    page.on("dialog", (dialog) => dialog.accept()),
+    await page.getByRole("button", { name: "기업 정보 수정완료" }).nth(1).click(),
+  ]);
 
-  page.on("dialog", (dialog) => dialog.accept());
-  await page.getByRole("button", { name: "공장 등록" }).click();
-
-  const afterFactoryListDataObj = await (
-    await page.waitForResponse((response) => response.url().includes("factories") && response.status() === 200)
-  ).json();
-
-  expect(beforeFactoryListDataObj.count + 1).toBe(afterFactoryListDataObj.count);
-
-  await expect(page.getByTestId("factory/list/factoryCardListPart")).toHaveCount(afterFactoryListDataObj.count);
-
-  await page.getByRole("button", { name: "공장수정" }).first().click();
-  await expect(
-    page.getByTestId("factory/list/factoryCardListPart").locator("div").filter({ hasText: "수정중" }).first()
-  ).toBeVisible();
-
-  await page.locator("input[name='factory_name']").fill("바뀐공장이름");
-  const anotherPopupPromise = page.waitForEvent("popup");
-
-  await page.getByRole("button", { name: "주소찾기" }).click();
-  const secondPopup = await anotherPopupPromise;
-  await secondPopup.waitForLoadState("networkidle");
-  await secondPopup.keyboard.insertText("부산");
-  await secondPopup.keyboard.press("Enter");
-  await page.waitForTimeout(1000);
-  await secondPopup.keyboard.press("Tab");
-  await secondPopup.keyboard.press("Tab");
-  await secondPopup.keyboard.press("Tab");
-  await secondPopup.keyboard.press("Tab");
-  await secondPopup.keyboard.press("Tab");
-  await secondPopup.keyboard.press("Tab");
-  await secondPopup.keyboard.press("Enter");
-  await page.waitForTimeout(500);
-
-  await page.locator("input[name='product']").fill("바뀐생산품");
-  await page.locator("input[name='male_number']").fill("99");
-  await page.locator("input[name='female_number']").fill("99");
-
-  await page.getByText("없음").first().click();
-  await page.getByText("없음").nth(1).click();
-  await page.locator('input[name="dormitory_etc"]').fill("새로운 dormitory");
-  await page.locator('input[name="bus_etc"]').fill("새로운 버스");
-
-  await page.getByRole("button", { name: "공장 수정" }).click();
-
-  await expect(page.getByTestId("factory/list/factoryCardListPart").last().getByText("바뀐공장이름")).toBeVisible();
-  await expect(page.getByTestId("factory/list/factoryCardListPart").last().getByText("새로운 dormitory")).toBeVisible();
-  await expect(page.getByTestId("factory/list/factoryCardListPart").last().getByText("새로운 버스")).toBeVisible();
-
-  const beforeDeleteCardCount = await page.getByTestId("factory/list/factoryCardListPart").count();
-  await page.getByTestId("factory/list/factoryCardListPart").last().getByRole("button", { name: "공장삭제" }).click();
-  await page.waitForLoadState("networkidle");
-
-  const afterDeleteCardCount = await page.getByTestId("factory/list/factoryCardListPart").count();
-  expect(beforeDeleteCardCount - 1).toBe(afterDeleteCardCount);
+  expect(companyRequest).toBeTruthy();
 });
