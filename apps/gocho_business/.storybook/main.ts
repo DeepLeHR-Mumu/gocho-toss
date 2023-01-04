bbdfc6c903d66a2a046dd981f28516ab1148e3d2
ahri <iwannaberealnerd@deeplehr.com>
2023-01-04 16:24:45 +0900

    [feat] 스토리북 추가완료

--- apps/gocho_business/.storybook/main.ts
+++ apps/gocho_business/.storybook/main.ts
@@ -5,11 +5,7 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 module.exports = {
   core: { builder: "webpack5" },
   stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
