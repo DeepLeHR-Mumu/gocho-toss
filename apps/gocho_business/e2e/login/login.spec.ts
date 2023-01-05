a8d28690ba9feec748f107ae15731e6e60f116a6
Vi <yuyukgh@deeplehr.com>
2023-01-05 11:30:36 +0900

    [Feat] 로그인 페이지 테스트코드 업데이트

--- apps/gocho_business/e2e/login/login.spec.ts
+++ apps/gocho_business/e2e/login/login.spec.ts
@@ -6,9 +6,55 @@ test.beforeEach(async ({ page }) => {
   await page.goto(INTERNAL_URL.HOME);
 });
 
-test("체크", async ({ page }) => {
+test("로그인 체크 및 로그인 이후 로그인페이지 블로킹", async ({ page }) => {
   await expect(page).toHaveURL(INTERNAL_URL.LOGIN);
   await page.getByPlaceholder("아이디(이메일)").fill("vi@deeplehr.com");
   await page.getByPlaceholder("비밀번호").fill("deeple1!");
+
+  const [loginResponse] = await Promise.all([
+    page.waitForResponse((response) => response.url().includes("/auth/login") && response.status() === 200),
+    page.waitForNavigation(),
+    await page.getByRole("button", { name: "로그인" }).click(),
+  ]);
+  expect(loginResponse.ok()).toBeTruthy();
+  await expect(page).toHaveURL(INTERNAL_URL.JD_LIST);
+
+  await page.goto(INTERNAL_URL.LOGIN);
+  await expect(page).toHaveURL(INTERNAL_URL.JD_LIST);
+});
+
+test("정보가 틀렸을 때", async ({ page }) => {
+  await page.getByPlaceholder("아이디(이메일)").fill("");
+  await page.getByPlaceholder("비밀번호").fill("deeple1!");
   await page.getByRole("button", { name: "로그인" }).click();
+  await expect(page.getByText("이메일을 입력해주세요.")).toBeVisible();
+
+  await page.getByPlaceholder("아이디(이메일)").fill("vi@deeplehr.com");
+  await page.getByPlaceholder("비밀번호").fill("");
+  await page.getByRole("button", { name: "로그인" }).click();
+  await expect(page.getByText("비밀번호를 입력해주세요.")).toBeVisible();
+
+  await page.getByPlaceholder("아이디(이메일)").fill("noId@deeplehr.com");
+  await page.getByPlaceholder("비밀번호").fill("1234");
+  await page.getByRole("button", { name: "로그인" }).click();
+  await expect(page.getByText("해당 유저가 존재하지 않습니다.")).toBeVisible();
+});
+
+test("로그인 안한 상태에서 타 페이지 접속시 블로킹", async ({ page }) => {
+  await page.goto(INTERNAL_URL.JD_LIST);
+  await expect(page).toHaveURL(INTERNAL_URL.LOGIN);
+  await page.goto(INTERNAL_URL.JD_UPLOAD);
+  await expect(page).toHaveURL(INTERNAL_URL.LOGIN);
+  await page.goto(INTERNAL_URL.FACTORY_LIST);
+  await expect(page).toHaveURL(INTERNAL_URL.LOGIN);
+  await page.goto(INTERNAL_URL.COMPANY_EDIT);
+  await expect(page).toHaveURL(INTERNAL_URL.LOGIN);
+  await page.goto(INTERNAL_URL.HELP);
+  await expect(page).toHaveURL(INTERNAL_URL.LOGIN);
+  await page.goto(INTERNAL_URL.HOME);
+  await expect(page).toHaveURL(INTERNAL_URL.LOGIN);
+  await page.goto(INTERNAL_URL.RECRUITER_LIST);
+  await expect(page).toHaveURL(INTERNAL_URL.LOGIN);
+  await page.goto(INTERNAL_URL.JD_EDIT(2));
+  await expect(page).toHaveURL(INTERNAL_URL.LOGIN);
 });
