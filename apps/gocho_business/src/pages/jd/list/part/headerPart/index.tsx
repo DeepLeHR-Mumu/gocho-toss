c2e656d1bdadef2b7a01eb46498968f6f2374cf4
Mumu <hsm9300@deeplehr.com>
2023-01-10 20:00:51 +0900

    [Fix] 버튼 관련 수정

--- apps/gocho_business/src/pages/jd/list/part/headerPart/index.tsx
+++ apps/gocho_business/src/pages/jd/list/part/headerPart/index.tsx
@@ -1,11 +1,11 @@
 import { FunctionComponent } from "react";
-import { BiRocket } from "react-icons/bi";
+import { FiArrowRight } from "react-icons/fi";
 import { useRouter } from "next/router";
 
 import { COLORS } from "shared-style/color";
+import { SharedButton } from "shared-ui/business/sharedButton";
 
 import { INTERNAL_URL } from "@/constants/url";
-import { CommonSquareButton } from "@/components/common";
 
 import { cssObj } from "./style";
 
@@ -18,18 +18,22 @@ export const HeaderPart: FunctionComponent = () => {
         <h2 css={cssObj.title}>공고 목록</h2>
         <p>공고를 수정하면 승인기간이 늘어날 수 있습니다</p>
       </div>
-      <CommonSquareButton
-        text="공고 등록"
-        iconObj={{ Icon: BiRocket, location: "left" }}
-        backgroundColor={`${COLORS.GRAY10}`}
-        fontColor={`${COLORS.GRAY100}`}
-        borderColor={`${COLORS.GRAY10}`}
-        onClickHandler={() => {
-          router.push({
-            pathname: INTERNAL_URL.JD_UPLOAD,
-          });
-        }}
-      />
+      <div css={cssObj.buttonWrapper}>
+        <SharedButton
+          radius="round"
+          size="medium"
+          text="공고 등록하러 가기"
+          isFullWidth
+          iconObj={{ icon: FiArrowRight, location: "right" }}
+          backgroundColor="#E9EEF9"
+          fontColor={`${COLORS.BLUE_FIRST40}`}
+          onClickHandler={() => {
+            router.push({
+              pathname: INTERNAL_URL.JD_UPLOAD,
+            });
+          }}
+        />
+      </div>
     </section>
   );
 };
