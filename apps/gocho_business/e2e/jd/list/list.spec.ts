c7586d19568513d6b4cbd38ca8534e4167c6b85e
Mumu <hsm9300@deeplehr.com>
2023-01-10 17:58:42 +0900

    [Fix] 테스트코드 수정

--- apps/gocho_business/e2e/jd/list/list.spec.ts
+++ apps/gocho_business/e2e/jd/list/list.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { INTERNAL_URL } from "@/constants/url";
 
 // import { businessLinkObj } from "shared-constant/e2e/internalURL";
 // import { loginTester } from "../../common/common.spec";
@@ -11,15 +12,21 @@ import { test, expect } from "@playwright/test";
 
 test.describe("공고 리스트 테스트", () => {
   test("공고 등록하기 버튼 테스트", async ({ page }) => {
+    await page.goto(INTERNAL_URL.JD_LIST);
+
     await page.getByText("공고 등록").click();
     expect(page.url().includes("jd/upload"));
   });
 
   test("타이틀 테스트", async ({ page }) => {
+    await page.goto(INTERNAL_URL.JD_LIST);
+
     await expect(page.locator("h2")).toHaveText("공고 목록");
   });
 
   test("공고 리스트 요소 개수 테스트", async ({ page }) => {
+    await page.goto(INTERNAL_URL.JD_LIST);
+
     const [jdListResponse] = await Promise.all([
       page.waitForResponse((response) => response.url().includes("/jds") && response.status() === 200),
     ]);
