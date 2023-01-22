f2b32f630096a25e19c6a92f94a9b0e37ad97658
ahri <iwannaberealnerd@deeplehr.com>
2023-01-22 18:52:28 +0900

    [feat] 액션 정상 작동 확인

--- apps/gocho_business/playwright.config.ts
+++ apps/gocho_business/playwright.config.ts
@@ -9,7 +9,7 @@ const config: PlaywrightTestConfig = {
   fullyParallel: true,
   globalSetup: require.resolve("./e2e/global-setup"),
   use: { storageState: "storageState.json" },
-  workers: 2,
+  // workers: 2,
   projects: [
     {
       name: "chromium",
