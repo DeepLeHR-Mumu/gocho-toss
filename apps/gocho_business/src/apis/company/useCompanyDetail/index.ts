67443b47c856a361f8c5bea5fdbd9bc4cc41a22a
Vi <yuyukgh@deeplehr.com>
2023-01-30 21:49:37 +0900

    [Fix] companyDetail import 순서변경

--- apps/gocho_business/src/apis/company/useCompanyDetail/index.ts
+++ apps/gocho_business/src/apis/company/useCompanyDetail/index.ts
@@ -2,10 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 
 import { axiosInstance } from "@/apis/useIsRefreshLock";
+import { ErrorResponseDef } from "@/types/errorType";
 
 import { companyDetailKeyObj, GetCompanyDetailDef, RequestObjDef, ResponseObjDef } from "./type";
 import { companyDetailSelector } from "./util";
-import { ErrorResponseDef } from "@/types/errorType";
 
 export const getCompanyDetail: GetCompanyDetailDef = async ({ queryKey: [{ requestObj }] }) => {
   const { data } = await axiosInstance.get(`/companies/${requestObj.companyId}`);
