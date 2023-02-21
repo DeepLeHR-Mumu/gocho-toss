4713478aba4fc3976db1a12d78b6bea5cb11fa9a
Mumu <hsm9300@deeplehr.com>
2023-02-21 15:17:49 +0900

    [HOTFIX] 기업 로고 수정 기능 핫픽스

--- apps/gocho_admin/src/api/company/useAddCompany/index.ts
+++ apps/gocho_admin/src/api/company/useAddCompany/index.ts
@@ -2,9 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 
 import { AdminResponseDef } from "shared-type/api/responseType";
-
 import { axiosInstance } from "@api/useAxiosInterceptor";
-
 import { PostCompanyDef, RequestObjDef, useAddCompanyProps } from "./type";
 
 export const postAddCompany: PostCompanyDef = async (requestObj) => {
