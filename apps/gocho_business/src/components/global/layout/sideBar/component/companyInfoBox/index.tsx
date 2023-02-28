b61ba5e0fb5e715fd703d83f184be8d14e15488a
Vi <yuyukgh@deeplehr.com>
2023-02-28 11:40:46 +0900

    [Fix] sideBar companyInfoBox 린트에 맞게 변경

--- apps/gocho_business/src/components/global/layout/sideBar/component/companyInfoBox/index.tsx
+++ apps/gocho_business/src/components/global/layout/sideBar/component/companyInfoBox/index.tsx
@@ -6,13 +6,11 @@ import defaultLogoSrc from "shared-image/global/common/default_company_logo.svg"
 import { CompanyInfoBoxProps } from "./type";
 import { cssObj } from "./style";
 
-export const CompanyInfoBox: FunctionComponent<CompanyInfoBoxProps> = ({ name, img }) => {
-  return (
-    <div css={cssObj.wrapper}>
-      <div css={cssObj.imageBox}>
-        <Image src={img || defaultLogoSrc} alt={name} fill sizes="1" />
-      </div>
-      <p css={cssObj.nameCSS}>{name}</p>
+export const CompanyInfoBox: FunctionComponent<CompanyInfoBoxProps> = ({ name, img }) => (
+  <div css={cssObj.wrapper}>
+    <div css={cssObj.imageBox}>
+      <Image src={img || defaultLogoSrc} alt={name} fill sizes="1" />
     </div>
-  );
-};
+    <p css={cssObj.nameCSS}>{name}</p>
+  </div>
+);
