e35fae99ef55cc772da492933b61996db2fb11bf
ahri <iwannaberealnerd@deeplehr.com>
2023-03-07 16:51:32 +0900

    [fix] CI 테스트 준비

--- shared/hooks/src/test/useViewCount.spec.ts
+++ shared/hooks/src/test/useViewCount.spec.ts
@@ -2,6 +2,5 @@ import { renderHook } from "@testing-library/react";
 import { useViewCount } from "../useViewCount";
 
 test("viewCount 테스트", async () => {
-  // localStorage.setItem("weiofjoi");
   renderHook(() => useViewCount({ id: 33 }));
 });
