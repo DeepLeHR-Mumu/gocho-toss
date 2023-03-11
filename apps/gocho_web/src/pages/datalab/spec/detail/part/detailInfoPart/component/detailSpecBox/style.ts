417a0b8f3b70105c722f3137b72bb2fd16bf3c33
Vi <yuyukgh@deeplehr.com>
2023-03-11 17:06:22 +0900

    [Feat] 기타, 커리어, 경력 타입변경에 맞는 조회내용으로 변경

--- apps/gocho_web/src/pages/datalab/spec/detail/part/detailInfoPart/component/detailSpecBox/style.ts
+++ apps/gocho_web/src/pages/datalab/spec/detail/part/detailInfoPart/component/detailSpecBox/style.ts
@@ -1,30 +1,26 @@
-import { css, SerializedStyles } from "@emotion/react";
+import { css } from "@emotion/react";
 import { COLORS } from "shared-style/color";
 
-interface WrapperDef {
-  (wide: boolean): SerializedStyles;
-}
-export const wrapper: WrapperDef = (wide) => {
-  return css`
-    width: ${wide === true ? "100%" : "fit-content"};
-    background-color: ${COLORS.GRAY90};
-    padding: 0.5rem 24px;
-    display: flex;
-    align-items: center;
-    border-radius: 2rem;
-    min-height: 2rem;
-    margin-bottom: 0.5rem;
-    :last-of-type {
-      margin-bottom: 0;
-    }
-  `;
-};
+export const wrapper = css`
+  width: 100%;
+  background-color: ${COLORS.GRAY90};
+  padding: 0.5rem 24px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 2rem;
+  min-height: 2rem;
+  margin-bottom: 0.5rem;
+  :last-of-type {
+    margin-bottom: 0;
+  }
+`;
 
 export const specStrCSS = css`
+  width: 100%;
   color: ${COLORS.GRAY10};
   font-weight: 400;
   word-break: break-all;
-  white-space: pre-line;
   font-size: 0.875rem;
   line-height: 1.8;
 `;
--- apps/gocho_web/src/pages/datalab/spec/detail/part/detailInfoPart/index.tsx
+++ apps/gocho_web/src/pages/datalab/spec/detail/part/detailInfoPart/index.tsx
@@ -4,69 +4,42 @@ import { AiOutlineEdit } from "react-icons/ai";
 
 import { DetailInfoPartProps } from "./type";
 
-import { NotExistingBox } from "../component/notExistingBox";
 import { DetailSpecBox } from "./component/detailSpecBox";
 import { container, infoBox, title } from "./style";
 
 export const DetailInfoPart: FunctionComponent<DetailInfoPartProps> = ({ detailData }) => {
+  const languageArr = detailData.language?.map((lang) => {
+    return `${lang.language} ${lang.test} ${lang.score}`;
+  });
+
   return (
     <section css={container}>
       <div css={infoBox}>
         <p css={title}>
           <BsStars /> 어학
         </p>
-        {detailData.language ? (
-          detailData.language.map((langCerti) => {
-            return (
-              <DetailSpecBox
-                key={langCerti.language}
-                specStr={`${langCerti.language} ${langCerti.test} ${langCerti.score}`}
-              />
-            );
-          })
-        ) : (
-          <NotExistingBox />
-        )}
+        <DetailSpecBox dataArr={languageArr} />
       </div>
 
       <div css={infoBox}>
         <p css={title}>
           <BsStars /> 수상
         </p>
-        {/* TODO: 추후 출력보고 코드 수정 필요 (award, career, etc) */}
-        {detailData.award ? (
-          detailData.award.map((element) => {
-            return <DetailSpecBox key={`AwardDetail${element}`} specStr={element} wide />;
-          })
-        ) : (
-          <NotExistingBox />
-        )}
+        <DetailSpecBox dataArr={detailData.award} />
       </div>
 
       <div css={infoBox}>
         <p css={title}>
           <BsStars /> 경력
         </p>
-        {detailData.career ? (
-          detailData.career.map((element) => {
-            return <DetailSpecBox key={`CareerDetail${element}`} specStr={element} wide />;
-          })
-        ) : (
-          <NotExistingBox />
-        )}
+        <DetailSpecBox dataArr={detailData.career} />
       </div>
 
       <div css={infoBox}>
         <p css={title}>
           <AiOutlineEdit /> 기타
         </p>
-        {detailData.etc ? (
-          detailData.etc.map((element) => {
-            return <DetailSpecBox key={`EtcDetail${element}`} specStr={element} wide />;
-          })
-        ) : (
-          <NotExistingBox />
-        )}
+        <DetailSpecBox dataArr={detailData.etc} />
       </div>
     </section>
   );
