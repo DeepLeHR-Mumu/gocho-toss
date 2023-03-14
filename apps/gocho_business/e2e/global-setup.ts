f52caa5afcf70726a105b6af8617e11205491989
ahri <iwannaberealnerd@deeplehr.com>
2023-03-14 16:13:08 +0900

    [fix] 조회수 로직 업데이트 완료

--- apps/gocho_business/e2e/global-setup.ts
+++ apps/gocho_business/e2e/global-setup.ts
@@ -5,7 +5,7 @@ import { INTERNAL_URL } from "@/constants/url";
 async function globalSetup() {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  const serverUrl = process.env.GOCHO_BUSINESS_URL || "http://localhost:3000";
+  const serverUrl = "http://localhost:3000";
   await page.goto(`${serverUrl}${INTERNAL_URL.LOGIN}`);
   await page.getByPlaceholder("아이디(이메일)").fill("yuyukgh@deeplehr.com");
   await page.getByPlaceholder("비밀번호").fill("deeple1!");
