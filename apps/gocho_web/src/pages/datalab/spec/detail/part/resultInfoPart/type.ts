e563f60580fc45eea2870fcb1799e03300a18e12
Mumu <hsm9300@deeplehr.com>
2023-03-16 01:13:36 +0900

    [Fix] 스펙 디테일 페이지 strongPointArr, weakPointArr 수정

--- apps/gocho_web/src/pages/datalab/spec/detail/part/resultInfoPart/type.ts
+++ apps/gocho_web/src/pages/datalab/spec/detail/part/resultInfoPart/type.ts
@@ -5,9 +5,36 @@ export interface ResultInfoPartProps {
     score: number | null;
     scoreCount: number;
     evals: {
-      strongPointArr: [string, number][];
-      weakPointArr: [string, number][];
+      strongPointArr: {
+        [key: string]: number;
+        NONE: number;
+        ATTENDANCE: number;
+        AGE: number;
+        EDUCATION_SCORE: number;
+        RELATED_CERT: number;
+        MANY_CERT: number;
+        LANGUAGE_SCORE: number;
+        AWARDS: number;
+        DEPARTMENT: number;
+        RELATED_CAREER: number;
+        LONG_CAREER: number;
+      };
+      weakPointArr: {
+        [key: string]: number;
+
+        NONE: number;
+        ATTENDANCE: number;
+        AGE: number;
+        EDUCATION_SCORE: number;
+        UNRELATED_CERT: number;
+        LESS_CERT: number;
+        LANGUAGE_SCORE: number;
+        AWARDS: number;
+        DEPARTMENT: number;
+        UNRELATED_CAREER: number;
+        SHORT_CAREER: number;
+      };
       feedbackArr: string[] | null;
-    } | null;
+    };
   };
 }
