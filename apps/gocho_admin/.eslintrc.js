8335515054bdae10e506b678f5bb99f73ca86ce5
ahri <iwannaberealnerd@deeplehr.com>
2023-03-18 22:31:02 +0900

    [feat] test 추가

--- apps/gocho_admin/.eslintrc.js
+++ apps/gocho_admin/.eslintrc.js
@@ -2,4 +2,8 @@ module.exports = {
   parserOptions: { tsconfigRootDir: __dirname, root: true },
   extends: ["deeple"],
   rules: { "import/no-unresolved": "off", "no-alert": "off" },
+  "import/no-extraneous-dependencies": [
+    "error",
+    { depnedencies: false, evDependencies: false, optionalDependencies: false, peerDependencies: false },
+  ],
 };
