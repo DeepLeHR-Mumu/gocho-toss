ec84dcff6308af855a7ad69556acd2e1282a6c6a
Mumu <hsm9300@deeplehr.com>
2023-03-23 11:14:10 +0900

    [Fix] 유닛 테스트 이름 변경

--- shared/util/src/test/dateConverter.spec.ts
+++ shared/util/src/test/dateConverter.spec.ts
@@ -12,19 +12,19 @@ describe("dDayCalculator unit 테스트", () => {
     expect(dDayCalculator(new Date().getTime() + 4)).toBe("D-DAY");
   });
   it("상시채용 test", () => {
-    const twoDaysLater = dayjs().set("year", 9999).valueOf();
-    expect(dDayCalculator(twoDaysLater)).toBe("상시채용");
+    const always = dayjs().set("year", 9999).valueOf();
+    expect(dDayCalculator(always)).toBe("상시채용");
   });
   it("48시간 후 D-DAY 테스트", () => {
     const twoDaysLater = dayjs().add(2, "day").valueOf();
     expect(dDayCalculator(twoDaysLater)).toBe("D-2");
   });
   it("지금 1일 12시간 이후", () => {
-    const oneDayOneHourLater = dayjs().add(1, "day").add(21, "hour");
-    expect(dDayCalculator(oneDayOneHourLater.valueOf())).toBe("D-2");
+    const oneDayTwelveHourLater = dayjs().add(1, "day").add(12, "hour");
+    expect(dDayCalculator(oneDayTwelveHourLater.valueOf())).toBe("D-2");
   });
   it("지금 1일 후", () => {
-    const oneDayOneHourLater = dayjs().add(1, "day");
-    expect(dDayCalculator(oneDayOneHourLater.valueOf())).toBe("D-1");
+    const oneDayLater = dayjs().add(1, "day");
+    expect(dDayCalculator(oneDayLater.valueOf())).toBe("D-1");
   });
 });
