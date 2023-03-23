41190ff95c9e85f1368040c96cb8dfaee4495d5f
Vi <yuyukgh@deeplehr.com>
2023-03-23 16:46:26 +0900

    [Feat] 리스트 페이지 테스트코드 업데이트

--- apps/gocho_admin/e2e/global-setup.ts
+++ apps/gocho_admin/e2e/global-setup.ts
@@ -7,8 +7,8 @@ async function globalSetup() {
   const page = await browser.newPage();
   const serverUrl = "http://localhost:3000";
   await page.goto(`${serverUrl}${INTERNAL_URL.LOGIN_URL}`);
-  await page.getByPlaceholder("아이디(이메일)").fill("admin@deeplehr.com");
-  await page.getByPlaceholder("비밀번호").fill("deeple1!");
+  await page.locator('input[name="email"]').fill("admin@deeplehr.com");
+  await page.locator('input[name="password"]').fill("deeple1!");
   await page.getByRole("button", { name: "로그인" }).click();
   await page.waitForLoadState("networkidle");
   await page.context().storageState({ path: "storageState.json" });
---
+++ apps/gocho_admin/e2e/jd/list/list.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+import { INTERNAL_URL } from "@/constant";
+
+test("공고 리스트 테스트", async ({ page }) => {
+  await page.goto(`${INTERNAL_URL.JD_LIST_URL}?page=1`);
+
+  const [jdListResponse] = await Promise.all([
+    page.waitForResponse((response) => response.url().includes("/jds") && response.status() === 200),
+  ]);
+  expect(jdListResponse.ok()).toBeTruthy();
+  await expect(page.locator("h2")).toHaveText("공고 목록");
+
+  await page.getByTestId("jd/list/jobCard").getByRole("link", { name: "수정" }).nth(0).click();
+  expect(page.url().includes("jd/edit"));
+});
