600f483312b71ef800adc9361c2548440a4e258a
Mumu <hsm9300@deeplehr.com>
2023-03-23 20:39:41 +0900

    [Fix] 스펙 등록 테스트 코드 수정

--- apps/gocho_web/e2e/datalab/spec/register/register.spec.ts
+++ apps/gocho_web/e2e/datalab/spec/register/register.spec.ts
@@ -163,6 +163,7 @@ test.describe("스펙등록 테스트", () => {
     await page.locator('textarea[name="career"]').fill("경력에 대한 서술...");
     await page.locator('textarea[name="etc"]').fill("기타사항에 대한 서술...");
 
+    await page.locator('button:has-text("스펙 등록하기")').click();
     await page.getByRole("link", { name: "스펙 리스트" }).click();
     await expect(page).toHaveURL(linkObj.SPEC_LIST_URL);
   });
