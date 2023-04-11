f83af52dbecb1ffe23f421212ba9ddd1d2c5cb17
ahri <iwannaberealnerd@deeplehr.com>
2023-04-11 18:11:01 +0900

    [env] 설정 완료

---
+++ apps/new_gocho/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { css } from "@emotion/react";
+
+export default function Home() {
+  return (
+    <p
+      css={css`
+        color: red;
+      `}
+    >
+      indexPage
+    </p>
+  );
+}
