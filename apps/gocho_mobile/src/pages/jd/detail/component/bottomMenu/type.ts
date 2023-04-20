1f9e5c05274fb9efd584014cdb56b0f5a06b74b7
Mumu <hsm9300@deeplehr.com>
2023-04-20 21:01:31 +0900

    [Fix] 불필요한 type 제거

--- apps/gocho_mobile/src/pages/jd/detail/component/bottomMenu/type.ts
+++ apps/gocho_mobile/src/pages/jd/detail/component/bottomMenu/type.ts
@@ -3,25 +3,9 @@ import { Dispatch, SetStateAction } from "react";
 export interface BottomMenuProps {
   jobDetailData: {
     id: number;
-    company: {
-      id: number;
-      name: string;
-      logoUrl: string;
-      youtubeUrl: string;
-      commentCount: number;
-    };
-    title: string;
-    cut: boolean;
-    startTime: string;
     endTime: string;
-    processArr: string[];
-    applyRouteArr: string[];
     applyUrl: string;
-    etcArr: string[];
-    bookmark: number;
     isBookmark: boolean;
-    view: number;
-    click: number;
   };
   setOpenComment: Dispatch<SetStateAction<boolean>>;
 }
