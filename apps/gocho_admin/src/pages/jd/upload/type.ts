200ed148c2fa3dd42225aff394794dfe16ccc9d4
Mumu <hsm9300@deeplehr.com>
2023-05-18 17:54:16 +0900

    [Fix] Admin 공고 등록 페이지 수정

--- apps/gocho_admin/src/pages/jd/upload/type.ts
+++ apps/gocho_admin/src/pages/jd/upload/type.ts
@@ -8,30 +8,28 @@ export interface JobFormValues {
   apply_route_arr: string;
   apply_url: string;
   etc_arr: string | null;
-  position_arr: {
-    middle: boolean;
-    high: boolean;
-    college: boolean;
-    four: boolean;
-    required_exp: string;
-    min_year: number | null;
-    max_year: number | null;
-    required_etc_arr: string | null;
-    contract_type: string;
-    conversion_rate: number | null;
-    task_main: string;
-    task_sub_arr: string[];
-    task_detail_arr: string;
-    rotation_arr: string[] | null;
-    place: {
-      type: string;
-      address_arr: string[] | null;
-      factory_arr: number[] | null;
-      etc: string | null;
-    };
-    hire_number: number;
-    pay_arr: string;
-    preferred_certi_arr: string[] | null;
-    preferred_etc_arr: string | null;
-  }[];
+  middle: boolean;
+  high: boolean;
+  college: boolean;
+  four: boolean;
+  required_exp: string;
+  min_year: number | null;
+  max_year: number | null;
+  required_etc_arr: string | null;
+  contract_type: string;
+  conversion_rate: number | null;
+  task_main: string;
+  task_sub_arr: string[];
+  task_detail_arr: string;
+  rotation_arr: string[] | null;
+  place: {
+    type: string;
+    address_arr: string[] | null;
+    factory_arr: number[] | null;
+    etc: string | null;
+  };
+  hire_number: number;
+  pay_arr: string;
+  preferred_certi_arr: string[] | null;
+  preferred_etc_arr: string | null;
 }
