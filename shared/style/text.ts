88b76573c6006327d6b740e74f3f650369c36bff
Mumu <hsm9300@deeplehr.com>
2023-05-31 20:35:09 +0900

    [Feat] shared-style/text 추가

---
+++ shared/style/text.ts
@@ -0,0 +1,79 @@
+export const TEXTS = {
+  TITLE1: `
+    font-weight: 400;
+    font-size: 0.75rem;
+    line-height: 1.33;
+  `,
+
+  TITLE2: `
+    font-weight: 500;
+    font-size: 0.75rem;
+    line-height: 1.33;
+  `,
+
+  TITLE3: `
+    font-weight: 400;
+    font-size: 0.875rem;
+    line-height: 1.28;
+  `,
+
+  TITLE4: `
+    font-weight: 500;
+    font-size: 0.875rem;
+    line-height: 1.28;
+  `,
+
+  TITLE5: `
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.25;
+  `,
+
+  TITLE6: `
+    font-weight: 500;
+    font-size: 1rem;
+    line-height: 1.25;
+  `,
+
+  TITLE7: `
+    font-weight: 700;
+    font-size: 1rem;
+    line-height: 1.25;
+  `,
+
+  TITLE8: `
+    font-weight: 500;
+    font-size: 1.125rem;
+    line-height: 1.22;
+  `,
+
+  TITLE9: `
+    font-weight: 700;
+    font-size: 1.125rem;
+    line-height: 1.22;
+  `,
+
+  TITLE10: `
+    font-weight: 600;
+    font-size: 1.25rem;
+    line-height: 1.16;
+  `,
+
+  TITLE11: `
+    font-weight: 700;
+    font-size: 1.75rem;
+    line-height: 1.14;
+  `,
+
+  TITLE12: `
+    font-weight: 700;
+    font-size: 2rem;
+    line-height: 1.125;
+  `,
+
+  TITLE13: `
+    font-weight: 700;
+    font-size: 2.5rem;
+    line-height: 1.1;
+  `,
+};
