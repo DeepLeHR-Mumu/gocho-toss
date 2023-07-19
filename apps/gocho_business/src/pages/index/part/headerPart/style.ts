ed08192f0d7965ff2f988291afe85caa1c93991c
Mumu <hsm9300@deeplehr.com>
2023-07-19 21:17:11 +0900

    [Feat] 메인페이지 상단 공지사항 추가

--- apps/gocho_business/src/pages/index/part/headerPart/style.ts
+++ apps/gocho_business/src/pages/index/part/headerPart/style.ts
@@ -1,6 +1,8 @@
 import { css } from "@emotion/react";
 
 import { NEWCOLORS } from "shared-style/color";
+import { TEXTS } from "shared-style/text";
+import { shorten } from "shared-style/common";
 
 export const cssObj = {
   headerContainer: css`
@@ -10,14 +12,24 @@ export const cssObj = {
     align-items: center;
     background-color: ${NEWCOLORS.WHITE};
     height: 3.5rem;
-    z-index: 30;
     box-shadow: 0 2px 10px 0 #00000008;
   `,
 
-  buttonContainer: css`
-    width: 1152px;
-    display: flex;
-    justify-content: flex-end;
+  noticeContainer: css`
+    width: 1152px !important;
+    display: flex !important;
+    justify-content: flex-start;
     margin: 0 auto;
+  `,
+
+  noticeType: css`
+    ${TEXTS.TITLE7};
+    width: 4rem;
+  `,
+
+  noticeTitle: css`
+    flex-grow: 1;
+    ${TEXTS.TITLE6};
+    ${shorten()};
   `,
 };
