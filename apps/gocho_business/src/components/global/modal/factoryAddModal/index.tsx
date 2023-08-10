bf79cd273387627083391442be1e4c4f1aaa611b
Mumu <hsm9300@deeplehr.com>
2023-08-10 18:25:30 +0900

    [Fix] 공장 부분 validation 수정

--- apps/gocho_business/src/components/global/modal/factoryAddModal/index.tsx
+++ apps/gocho_business/src/components/global/modal/factoryAddModal/index.tsx
@@ -78,16 +78,18 @@ export const FactoryAddModal: FunctionComponent = () => {
             <strong css={commonCssObj.inputTitle(false)}>공장 명칭</strong>
             <input
               css={commonCssObj.input(37.5, false)}
-              {...register("factory_name", { maxLength: 50, required: true })}
-              placeholder="고초대졸 제1공장 (최대 50자)"
+              maxLength={10}
+              {...register("factory_name", { maxLength: 10, required: true })}
+              placeholder="고초대졸 제1공장 (최대 10자)"
             />
           </div>
           <div css={commonCssObj.container}>
             <strong css={commonCssObj.inputTitle(false)}>생산품</strong>
             <input
               css={commonCssObj.input(37.5, false)}
-              {...register("product", { maxLength: 50, required: true })}
-              placeholder="공장 주 생산품 (최대 50자)"
+              maxLength={30}
+              {...register("product", { maxLength: 30, required: true })}
+              placeholder="공장 주 생산품 (최대 30자)"
             />
           </div>
           <div css={commonCssObj.container}>
@@ -144,8 +146,11 @@ export const FactoryAddModal: FunctionComponent = () => {
                 </SharedRadioButton>
               </div>
               <input
+                css={commonCssObj.input(37.5, false)}
+                placeholder="보충 설명(선택, 최대 50자)"
+                maxLength={50}
                 {...register("bus_etc", {
-                  maxLength: 70,
+                  maxLength: 50,
                   validate: (value) => {
                     if (value) {
                       return value.trim().length !== 0;
@@ -153,9 +158,6 @@ export const FactoryAddModal: FunctionComponent = () => {
                     return true;
                   },
                 })}
-                css={commonCssObj.input(37.5, false)}
-                placeholder="보충 설명(선택)"
-                maxLength={70}
               />
             </div>
           </div>
@@ -179,8 +181,11 @@ export const FactoryAddModal: FunctionComponent = () => {
                 </SharedRadioButton>
               </div>
               <input
+                css={commonCssObj.input(37.5, false)}
+                placeholder="보충 설명(선택, 최대 50자)"
+                maxLength={50}
                 {...register("dormitory_etc", {
-                  maxLength: 70,
+                  maxLength: 50,
                   validate: (value) => {
                     if (value) {
                       return value.trim().length !== 0;
@@ -188,9 +193,6 @@ export const FactoryAddModal: FunctionComponent = () => {
                     return true;
                   },
                 })}
-                css={commonCssObj.input(37.5, false)}
-                placeholder="보충 설명(선택)"
-                maxLength={70}
               />
             </div>
           </div>
