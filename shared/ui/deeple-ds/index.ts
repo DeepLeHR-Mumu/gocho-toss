9698dc821e8bf33658ce9fd5a3fff12155d49872
wooyi <wychoi@deeplehr.com>
2023-08-14 17:25:13 +0900

    [Chore] root index 추가

---
+++ shared/ui/deeple-ds/index.ts
@@ -0,0 +1,15 @@
+import Button from "./Button";
+import Checkbox from "./Checkbox";
+import Chip from "./Chip";
+import Divider from "./Divider";
+import DropDown from "./DropDown";
+import Input from "./Input";
+import Modal from "./Modal";
+import Popup from "./Modal/Popup";
+import Profile from "./Profile";
+import Radio from "./Radio";
+import SearchBar from "./SearchBar";
+import Switch from "./Switch";
+import Toast from "./Toast";
+
+export { Button, Checkbox, Chip, Divider, DropDown, Input, Modal, Popup, Profile, Radio, SearchBar, Switch, Toast };
