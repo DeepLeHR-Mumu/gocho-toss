69e047dd1456b3126dad9c466efbe55612337edd
wooyi <wychoi@deeplehr.com>
2023-08-18 15:00:33 +0900

    [Chore] 필요한 모듈 추가

--- apps/new_gocho/next.config.js
+++ apps/new_gocho/next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [`cdn.gocho-back.com`, "devcdn.gocho-back.com"],
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [1256],
+    imageSizes: [],
+  },
   compiler: {
     emotion: true,
   },
+  pageExtensions: ["page.tsx"],
   transpilePackages: [
     "shared-api",
     "shared-constant",
