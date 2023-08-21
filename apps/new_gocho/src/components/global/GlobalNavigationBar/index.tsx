c81202d3e064588533aee2bd13ecea68552af85b
Mumu <hsm9300@deeplehr.com>
2023-08-21 21:18:35 +0900

    [Feat] GNB에 Layout 추가

--- apps/new_gocho/src/components/global/GlobalNavigationBar/index.tsx
+++ apps/new_gocho/src/components/global/GlobalNavigationBar/index.tsx
@@ -6,6 +6,7 @@ import { SearchBar, Button } from "shared-ui/deeple-ds";
 import { URL } from "@/pages/constants";
 import logoWhite from "@/public/logoWhite.svg";
 
+import { Layout } from "@/components";
 import { LoginModal } from "../../modal/LoginModal";
 import { SearchModal } from "../../modal/SearchModal";
 import { cssObj } from "./style";
@@ -17,52 +18,54 @@ export const GlobalNavigationBar = () => {
   return (
     <>
       <header css={cssObj.wrapper}>
-        <div css={cssObj.titleArea}>
-          <Link href={URL.MAIN} css={cssObj.logoWrapper}>
-            <Image src={logoWhite} alt="고초대졸_로고" />
-          </Link>
-          <div css={cssObj.searchBarWrapper}>
-            <SearchBar
-              onClick={() => {
-                setSearchModal(true);
-              }}
-            />
+        <Layout>
+          <div css={cssObj.titleArea}>
+            <Link href={URL.MAIN} css={cssObj.logoWrapper}>
+              <Image src={logoWhite} alt="고초대졸_로고" />
+            </Link>
+            <div css={cssObj.searchBarWrapper}>
+              <SearchBar
+                onClick={() => {
+                  setSearchModal(true);
+                }}
+              />
+            </div>
           </div>
-        </div>
-        <div css={cssObj.navigationArea}>
-          <nav>
-            <ul css={cssObj.navigationWrapper}>
-              <li>
-                <Link href={URL.JOBS_LIST}>채용공고</Link>
-              </li>
-              <li>
-                <Link href={URL.COMPANY}>기업정보</Link>
-              </li>
-              <li>
-                <Link href={URL.COMMUNITY}>커뮤니티</Link>
-              </li>
-            </ul>
-          </nav>
-          <div css={cssObj.etcWrapper}>
-            <a
-              href="https://gocho.biz/?utm_source=gochodaejoldotcom&utm_medium=GNB"
-              target="_blank"
-              rel="noreferrer"
-              css={cssObj.businessServicebutton}
-            >
-              기업서비스
-            </a>
-            <Button
-              size="small"
-              css={cssObj.loginButton}
-              onClick={() => {
-                setLoginModal(true);
-              }}
-            >
-              로그인/회원가입
-            </Button>
+          <div css={cssObj.navigationArea}>
+            <nav>
+              <ul css={cssObj.navigationWrapper}>
+                <li>
+                  <Link href={URL.JOBS_LIST}>채용공고</Link>
+                </li>
+                <li>
+                  <Link href={URL.COMPANY}>기업정보</Link>
+                </li>
+                <li>
+                  <Link href={URL.COMMUNITY}>커뮤니티</Link>
+                </li>
+              </ul>
+            </nav>
+            <div css={cssObj.etcWrapper}>
+              <a
+                href="https://gocho.biz/?utm_source=gochodaejoldotcom&utm_medium=GNB"
+                target="_blank"
+                rel="noreferrer"
+                css={cssObj.businessServicebutton}
+              >
+                기업서비스
+              </a>
+              <Button
+                size="small"
+                css={cssObj.loginButton}
+                onClick={() => {
+                  setLoginModal(true);
+                }}
+              >
+                로그인/회원가입
+              </Button>
+            </div>
           </div>
-        </div>
+        </Layout>
       </header>
       {loginModal && (
         <LoginModal
