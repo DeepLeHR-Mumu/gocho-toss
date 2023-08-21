c58cecb4e7041ad574fb9f6ea7186d80b69166bf
Mumu <hsm9300@deeplehr.com>
2023-08-21 22:27:32 +0900

    [Fix] GNB 및 푸터 수정

--- apps/new_gocho/src/components/Layout/style.ts
+++ apps/new_gocho/src/components/Layout/style.ts
@@ -2,7 +2,7 @@ import { css } from "@emotion/react";
 
 export const layout = css`
   width: 100%;
-  max-width: 1112px;
+  max-width: 1144px;
   padding: 0 2rem;
   margin: auto;
   transition: padding 0.2s ease;
--- apps/new_gocho/src/pages/index.page.tsx
+++ apps/new_gocho/src/pages/index.page.tsx
@@ -1,9 +1,15 @@
 import { NextPage } from "next";
 
+import { Layout } from "@/components";
+
 const Home: NextPage = () => {
   return (
     <main>
-      <p>indexPage</p>
+      <Layout>
+        <h2>실시간 HOT 공고</h2>
+        <h2>키워드 별 기업 모아보기</h2>
+        <h2>인기 QnA 살펴보기</h2>
+      </Layout>
     </main>
   );
 };
