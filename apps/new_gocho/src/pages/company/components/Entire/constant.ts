7bf179de4c0d0e736f31050eee4024e93c6cd308
= <yeongi@deeplehr.com>
2023-09-01 15:35:29 +0900

    [Fix] 빈파일 제거 및 상수 제거

--- apps/new_gocho/src/pages/company/components/Entire/constant.ts
+++
@@ -1,2 +0,0 @@
-export const ENTIRE_COMPANY_TEXT = "전체보기";
-export const ENTIRE_COMPANY_TITLE = "전체기업";
--- apps/new_gocho/src/pages/company/components/Entire/index.tsx
+++ apps/new_gocho/src/pages/company/components/Entire/index.tsx
@@ -2,14 +2,13 @@ import Link from "next/link";
 
 import { CompanyRow } from "@/components";
 import { HeaderTitle } from "../common/HeaderTitle";
-import { ENTIRE_COMPANY_TEXT, ENTIRE_COMPANY_TITLE } from "./constant";
 
 import { useCompanyArr } from "@/apis/company";
 
 import { cssObj } from "./style";
 
 // TODO: 전체 기업 보기 리스트 링크 논의
-// TODO: 팔로우 로직 처리 질문하기
+// TODO: 팔로우 로직 넣기
 
 export const EntireCompany = () => {
   const { data: entireCompanyArr } = useCompanyArr({
@@ -20,9 +19,9 @@ export const EntireCompany = () => {
   return (
     <section css={cssObj.entrieCompanyContainer}>
       <div css={cssObj.headerBox}>
-        <HeaderTitle title={ENTIRE_COMPANY_TITLE} />
+        <HeaderTitle title="전체기업" />
         <Link href="company/list" css={cssObj.etrieCompanyLinkText}>
-          {ENTIRE_COMPANY_TEXT}
+          전체보기
         </Link>
       </div>
 
