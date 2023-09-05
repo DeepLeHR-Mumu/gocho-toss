908ef1592bd358200c6f38cd51b6c4e2d9a9385e
wooyi <wychoi@deeplehr.com>
2023-09-05 10:17:16 +0900

    [Feat] CompanyRow 에 북마크 기능 추가

--- apps/new_gocho/src/components/common/CompanyRow/index.tsx
+++ apps/new_gocho/src/components/common/CompanyRow/index.tsx
@@ -1,30 +1,56 @@
+import { useState } from "react";
 import Link from "next/link";
 
 import { Profile } from "shared-ui/deeple-ds";
 import { FollowButton } from "shared-ui/deeple-ds/FollowButton";
 
+import { useUserProfile } from "@/apis/auth";
+import { useCompanyBookmarkToggle } from "@/apis/company";
 import { useGetDeviceType } from "@/globalStates";
+import { LoginModal } from "@/components/modal/LoginModal";
 
 import { CompanyRowProps } from "./type";
 import { cssObj } from "./style";
 
-export const CompanyRow = ({ id, logo, name, size, industry, follow, border }: CompanyRowProps) => {
+export const CompanyRow = ({ id, logo, name, size, industry, bookmark, border }: CompanyRowProps) => {
+  const [loginModal, setLoginModal] = useState(false);
   const { isMobile } = useGetDeviceType();
+  const { data: userData } = useUserProfile();
+  const { mutate: companyBookmarkToggle } = useCompanyBookmarkToggle();
+
+  const bookmarkToggleHandler = () => {
+    if (!userData) {
+      setLoginModal(true);
+      return;
+    }
+    companyBookmarkToggle({ companyId: id });
+  };
 
   return (
-    <Link css={cssObj.wrapper(!!border)} href={`/company/${id}/detail`}>
-      <Profile size={isMobile ? 52 : 60} src={logo} />
-      <div css={cssObj.infoWrapper}>
-        <h3 css={cssObj.companyName}>{name}</h3>
-        <span css={cssObj.companyCategory}>
-          {size} · {industry}
-        </span>
+    <>
+      <div css={cssObj.wrapper(!!border)}>
+        <Profile size={isMobile ? 52 : 60} src={logo} />
+        <div css={cssObj.infoWrapper}>
+          <Link href={`/company/${id}/detail`} css={cssObj.companyName}>
+            {name}
+          </Link>
+          <span css={cssObj.companyCategory}>
+            {size} · {industry}
+          </span>
+        </div>
+        {bookmark && (
+          <FollowButton color={bookmark.state ? "follow" : "unfollow"} onClick={bookmarkToggleHandler}>
+            {bookmark.state ? "팔로잉" : "팔로우"}
+          </FollowButton>
+        )}
       </div>
-      {follow && (
-        <FollowButton color={follow.state ? "follow" : "unfollow"} onClick={follow.onClick}>
-          {follow.state ? "팔로잉" : "팔로우"}
-        </FollowButton>
+      {loginModal && (
+        <LoginModal
+          close={() => {
+            setLoginModal(false);
+          }}
+        />
       )}
-    </Link>
+    </>
   );
 };
