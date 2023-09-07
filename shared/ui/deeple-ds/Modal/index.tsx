d3f504446ee25e5b41cabf6b0e7de5a42f85aef5
= <yeongi@deeplehr.com>
2023-09-07 15:56:07 +0900

    [Fix] Modal document undefined 이슈 해결

--- shared/ui/deeple-ds/Modal/index.tsx
+++ shared/ui/deeple-ds/Modal/index.tsx
@@ -1,17 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 import { ModalProps } from "./type";
 import { cssObj } from "./style";
 
 export const Modal = ({ portalId = "portal", children, ...props }: ModalProps) => {
+  const [isClient, setIsClient] = useState(false);
+
   useEffect(() => {
+    setIsClient(true);
+
     document.body.style.overflow = "hidden";
 
     return () => {
       document.body.style.overflow = "auto";
     };
   }, []);
+
+  if (!isClient) {
+    return null;
+  }
 
   return createPortal(
     <div css={cssObj.layout} {...props}>
