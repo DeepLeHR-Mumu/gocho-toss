5db2983ccda56223138b98b08ea267a3a35575dd
Mumu <hsm9300@deeplehr.com>
2023-09-12 17:34:38 +0900

    [Fix] global 카카오 관련 오류 수정

--- apps/new_gocho/src/pages/company/detail/part/CompanyInfoPart/component/CommonInfo/component/KakaoMap/index.tsx
+++ apps/new_gocho/src/pages/company/detail/part/CompanyInfoPart/component/CommonInfo/component/KakaoMap/index.tsx
@@ -3,6 +3,13 @@ import { FunctionComponent, useEffect } from "react";
 import { KakaoMapProps } from "./type";
 import { cssObj } from "./style";
 
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    kakao: any;
+  }
+}
+
 export const KakaoMap: FunctionComponent<KakaoMapProps> = ({ address }) => {
   useEffect(() => {
     const mapScript = document.createElement("script");
@@ -13,14 +20,14 @@ export const KakaoMap: FunctionComponent<KakaoMapProps> = ({ address }) => {
     document.head.appendChild(mapScript);
 
     const onLoadKakaoMap = () => {
-      window.Kakao.maps.load(() => {
+      window.kakao.maps.load(() => {
         const container = document.getElementById("map");
         const options = {
-          center: new window.Kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
+          center: new window.kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
           level: 6,
         };
-        const map = new window.Kakao.maps.Map(container, options);
-        const geocoder = new window.Kakao.maps.services.Geocoder();
+        const map = new window.kakao.maps.Map(container, options);
+        const geocoder = new window.kakao.maps.services.Geocoder();
 
         // 주소로 좌표를 검색합니다
         geocoder.addressSearch(
@@ -28,11 +35,11 @@ export const KakaoMap: FunctionComponent<KakaoMapProps> = ({ address }) => {
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
           (result: any, status: any) => {
             // 정상적으로 검색이 완료됐으면
-            if (status === window.Kakao.maps.services.Status.OK) {
-              const coords = new window.Kakao.maps.LatLng(result[0].y, result[0].x);
+            if (status === window.kakao.maps.services.Status.OK) {
+              const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
 
               // 결과값으로 받은 위치를 마커로 표시합니다
-              const marker = new window.Kakao.maps.Marker({
+              const marker = new window.kakao.maps.Marker({
                 map,
                 position: coords,
               });
@@ -41,7 +48,7 @@ export const KakaoMap: FunctionComponent<KakaoMapProps> = ({ address }) => {
               marker.setMap(map);
 
               // 인포윈도우로 장소에 대한 설명을 표시합니다
-              const infoWindow = new window.Kakao.maps.InfoWindow({
+              const infoWindow = new window.kakao.maps.InfoWindow({
                 content: `<div style='height: 3rem; padding: 0.5rem; text-align: center; font-size: 0.875rem;'>${address}</div>`,
               });
               infoWindow.open(map, marker);
