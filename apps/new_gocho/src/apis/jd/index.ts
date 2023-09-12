3115d0b6fc4abe1d910287747451415edb07936c
wooyi <wychoi@deeplehr.com>
2023-09-12 19:22:08 +0900

    [Feat] useJdCount 추가

--- apps/new_gocho/src/apis/jd/index.ts
+++ apps/new_gocho/src/apis/jd/index.ts
@@ -5,3 +5,4 @@ export { useJdCountInfo } from "./useJdCountInfo";
 export { useJdBookmarkToggle } from "./useJdBookmarkToggle";
 export { useUserJdBookmarkArr } from "./useUserJdBookmarkArr";
 export { useInfiniteUserJdBookmarkArr } from "./useInfiniteUserJdBookmarkArr";
+export { useJdCount } from "./useJdCount";
---
+++ apps/new_gocho/src/apis/jd/useJdCount/index.ts
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { jdCountKeyObj, JdCountRequestObjDef } from "@/constants/queryKeyFactory/jd/jdCountKeyObj";
+
+import { axiosInstance } from "../../axiosInstance";
+import { GetJdCountDef } from "./type";
+
+export const getJdCount: GetJdCountDef = async ({ queryKey: [{ requestObj }] }) => {
+  const { data } = await axiosInstance.get("/jds/count", { params: requestObj });
+  return data;
+};
+
+export const useJdCount = (requestObj: JdCountRequestObjDef) => {
+  return useQuery({
+    queryKey: jdCountKeyObj.jdCount(requestObj),
+    queryFn: getJdCount,
+  });
+};
