349285455feb78a969c65f8bb7f554336d608c0f
= <yeongi@deeplehr.com>
2023-09-15 12:22:29 +0900

    [Feat] 마이페이지 history 수정 및 api 타입 추가

--- apps/new_gocho/src/pages/mypage/part/BookmarkPart/part/HistoryPart/index.tsx
+++ apps/new_gocho/src/pages/mypage/part/BookmarkPart/part/HistoryPart/index.tsx
@@ -14,59 +14,68 @@ export const HistoryPart = () => {
   const [currentMenu, setCurrentMenu] = useState<RecentMenu>("기업");
 
   const { data: userInfo } = useUserInfo();
-  const { data: companyArr } = useUserCompanyHistoryArr({
+  const { data: companyHistoryObj } = useUserCompanyHistoryArr({
     userId: userInfo?.id,
   });
-  const { data: jdArr } = useUserJdHistoryArr({
+  const { data: jdHistoryObj } = useUserJdHistoryArr({
     userId: userInfo?.id,
   });
 
   return (
     <>
-      {!companyArr && !jdArr && <NoListCard text="최근 본 내역이 없습니다." />}
-      {companyArr && jdArr && (
-        <>
-          <div>
-            <div css={cssObj.menuWrapper}>
-              <button
-                type="button"
-                onClick={() => setCurrentMenu("기업")}
-                css={currentMenu === "기업" ? cssObj.selectedButton : cssObj.defaultButton}
-              >
-                기업
-              </button>
-              <button
-                type="button"
-                onClick={() => setCurrentMenu("공고")}
-                css={currentMenu === "공고" ? cssObj.selectedButton : cssObj.defaultButton}
-              >
-                공고
-              </button>
-            </div>
-          </div>
-          <div css={cssObj.listWrapper}>
-            {currentMenu === "기업" &&
-              companyArr.companyHistoryDataArr.map(({ id, industry, name, logoUrl, size }) => (
-                <CompanyRow
-                  key={id}
-                  id={id}
-                  size={size}
-                  logo={logoUrl || ""}
-                  name={name}
-                  industry={industry}
-                  border
-                  bookmark={{
-                    state: true,
-                  }}
-                />
-              ))}
-            {currentMenu === "공고" &&
-              jdArr.userJdHistoryArr.map(({ id, title, endTime, company }) => (
-                <JdRow jdId={id} key={id} dueDate={endTime} jdTitle={title} bookmarked companyName={company.name} />
-              ))}
-          </div>
-        </>
-      )}
+      <div>
+        <div css={cssObj.menuWrapper}>
+          <button
+            type="button"
+            onClick={() => setCurrentMenu("기업")}
+            css={currentMenu === "기업" ? cssObj.selectedButton : cssObj.defaultButton}
+          >
+            기업
+          </button>
+          <button
+            type="button"
+            onClick={() => setCurrentMenu("공고")}
+            css={currentMenu === "공고" ? cssObj.selectedButton : cssObj.defaultButton}
+          >
+            공고
+          </button>
+        </div>
+      </div>
+      <div css={cssObj.listWrapper}>
+        {currentMenu === "기업" && companyHistoryObj?.companyHistoryDataArr.length ? (
+          companyHistoryObj?.companyHistoryDataArr.map(({ id, industry, name, logoUrl, size }) => (
+            <CompanyRow
+              key={id}
+              company={{
+                id,
+                size,
+                industry,
+                name,
+                logo: logoUrl || "",
+                bookmark: { state: true },
+              }}
+            />
+          ))
+        ) : (
+          <NoListCard text="최근 본 내역이 없습니다." />
+        )}
+        {currentMenu === "공고" && jdHistoryObj?.userJdHistoryArr.length ? (
+          jdHistoryObj.userJdHistoryArr.map(({ id, title, endTime, company, isBookmark }) => (
+            <JdRow
+              key={id}
+              jd={{
+                jdId: id,
+                dueDate: endTime,
+                jdTitle: title,
+                companyName: company.name,
+                bookmarked: isBookmark,
+              }}
+            />
+          ))
+        ) : (
+          <NoListCard text="최근 본 내역이 없습니다." />
+        )}
+      </div>
     </>
   );
 };
