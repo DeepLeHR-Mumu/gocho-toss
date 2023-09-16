9a9a9450ac2d8215253050512fde982b51b0ba16
= <yeongi@deeplehr.com>
2023-09-16 15:46:10 +0900

    [Fix] 연봉정보-기타정보 없을 시 조건 부 렌더링

--- apps/new_gocho/src/pages/company/detail/part/CompanyInfoPart/component/PayInfo/index.tsx
+++ apps/new_gocho/src/pages/company/detail/part/CompanyInfoPart/component/PayInfo/index.tsx
@@ -6,22 +6,24 @@ import { PayInfoProps } from "./type";
 import { cssObj } from "./style";
 
 export const PayInfo = ({ payStart, payAvg, payDesc }: PayInfoProps) => (
-    <section css={commonCssObj.box}>
-      <h3 css={commonCssObj.title}>연봉 정보</h3>
-      <Divider />
-      <div css={cssObj.contentsWrapper}>
-        <div css={cssObj.rowWrapper}>
-          <span>평균 초봉</span>
-          <span css={cssObj.pay}>{payStart} 만원</span>
-        </div>
-        <div css={cssObj.rowWrapper}>
-          <span>평균 연봉</span>
-          <span css={cssObj.pay}>{payAvg} 만원</span>
-        </div>
+  <section css={commonCssObj.box}>
+    <h3 css={commonCssObj.title}>연봉 정보</h3>
+    <Divider />
+    <div css={cssObj.contentsWrapper}>
+      <div css={cssObj.rowWrapper}>
+        <span>평균 초봉</span>
+        <span css={cssObj.pay}>{payStart} 만원</span>
+      </div>
+      <div css={cssObj.rowWrapper}>
+        <span>평균 연봉</span>
+        <span css={cssObj.pay}>{payAvg} 만원</span>
+      </div>
+      {payDesc.length > 0 && (
         <div css={cssObj.rowWrapper}>
           <span>기타정보</span>
           <span>{payDesc}</span>
         </div>
-      </div>
-    </section>
-  );
+      )}
+    </div>
+  </section>
+);
