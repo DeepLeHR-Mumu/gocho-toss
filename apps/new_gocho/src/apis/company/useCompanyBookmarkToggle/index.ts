296afd395aba7070ddbad5c7fd27aa72e4d3787b
Mumu <hsm9300@deeplehr.com>
2023-09-20 17:49:15 +0900

    [Fix] 키워드 기업 팔로우 문구 추가

--- apps/new_gocho/src/apis/company/useCompanyBookmarkToggle/index.ts
+++ apps/new_gocho/src/apis/company/useCompanyBookmarkToggle/index.ts
@@ -5,6 +5,7 @@ import { companyArrKeyObj } from "@/constants/queryKeyFactory/company/arrKeyObj"
 import { companyDetailKeyObj } from "@/constants/queryKeyFactory/company/companyDetailKeyObj";
 import { userBookmarkKeyObj } from "@/constants/queryKeyFactory/bookmark/bookmarkKeyObj";
 import { jdDetailKeyObj } from "@/constants/queryKeyFactory/jd/jdDetailKeyObj";
+import { companyKeywordArrKeyObj } from "@/apis/keyword/useCompanyKeywordArr/type";
 
 import { axiosInstance } from "../../axiosInstance";
 import { PostCompanyBookmarkDef, RequestObjDef } from "./type";
@@ -23,6 +24,7 @@ export const useCompanyBookmarkToggle = () => {
       queryClient.invalidateQueries(companyDetailKeyObj.all);
       queryClient.invalidateQueries(userBookmarkKeyObj.companyAll);
       queryClient.invalidateQueries(jdDetailKeyObj.all);
+      queryClient.invalidateQueries(companyKeywordArrKeyObj.all);
     },
   });
 };
