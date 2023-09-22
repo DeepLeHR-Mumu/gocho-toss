9d70a6d2a499545add0d76c52dd59a91a2dc4229
zac <yeongi@deeplehr.com>
2023-09-22 13:41:03 +0900

    [Feat] 회사 로고 없을 때 기본 프로필 표시

--- shared/ui/deeple-ds/Profile/index.tsx
+++ shared/ui/deeple-ds/Profile/index.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
+import defaultLogo from "shared-image/global/common/default_company_logo.svg";
+
 import { ProfileProps } from "./type";
 import { cssObj } from "./style";
 
 export const Profile = ({ src, size = 100, onClick, altText, className }: ProfileProps) => (
   // TODO 실제 개발 시 적용되는 지 테스트 필요
   <div css={cssObj.profile(onClick !== undefined, size)}>
-    <Image src={src} alt={altText} width={size} height={size} onClick={onClick} className={className} />
+    <Image src={src || defaultLogo} alt={altText} width={size} height={size} onClick={onClick} className={className} />
   </div>
 );
