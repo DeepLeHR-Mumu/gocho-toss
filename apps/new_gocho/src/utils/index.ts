b27c8099b1bda13ccfe83025a49b2b1ce75d007e
Mumu <hsm9300@deeplehr.com>
2023-10-04 14:18:47 +0900

    [Fix] 리뷰어에 프론트엔드 팀 추가, 사용하지 않는 함수 삭제

--- apps/new_gocho/src/utils/index.ts
+++ apps/new_gocho/src/utils/index.ts
@@ -8,18 +8,6 @@ export const isExpired = (date: Date) => {
   return date.getTime() < today.getTime();
 };
 
-export const getDayUntilExpiry = (expiryTime: Date) => {
-  const targetExpiryTime = new Date(expiryTime);
-  targetExpiryTime.setHours(0, 0, 0, 0);
-
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
-
-  const timestampUntilExpiry = targetExpiryTime.getTime() - today.getTime();
-
-  return Math.floor(timestampUntilExpiry / 1000 / 60 / 60 / 24);
-};
-
 export const isStringArray = (param: unknown): param is string[] => {
   if (!Array.isArray(param)) {
     return false;
