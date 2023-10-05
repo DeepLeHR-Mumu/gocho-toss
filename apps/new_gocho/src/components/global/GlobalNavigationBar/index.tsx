7e7bcc47af0f31c5dd1a847229786a988f2db3a1
Mumu <hsm9300@deeplehr.com>
2023-10-05 16:07:03 +0900

    [Fix] QA 사항들 적용

--- apps/new_gocho/src/components/global/GlobalNavigationBar/index.tsx
+++ apps/new_gocho/src/components/global/GlobalNavigationBar/index.tsx
@@ -3,12 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useQueryClient } from "@tanstack/react-query";
-import { FiBell, FiSearch, FiUser } from "react-icons/fi";
 
 import { SearchBar, DropDown, Profile } from "shared-ui/deeple-ds";
 
 import { useUserInfo } from "@/apis/auth";
-import { useGetDeviceType } from "@/globalStates";
 import { searchFunnelEvent } from "@/ga/search";
 import { INTERNAL_URL } from "@/pages/constants";
 import logoWhite from "@/public/image/logo/gocho/white.svg";
@@ -31,7 +29,6 @@ export const GlobalNavigationBar = () => {
     wrapper,
     titleArea,
     logoWrapper,
-    mobileIcon,
     searchBarWrapper,
     navigationArea,
     navigationWrapper,
@@ -44,8 +41,6 @@ export const GlobalNavigationBar = () => {
 
   const [loginModal, setLoginModal] = useState(false);
   const [searchModal, setSearchModal] = useState(false);
-
-  const { isMobile } = useGetDeviceType();
 
   const { data: userData, isSuccess } = useUserInfo();
 
@@ -69,155 +64,128 @@ export const GlobalNavigationBar = () => {
     <>
       <header css={wrapper}>
         <Layout>
-          {isMobile ? (
-            <div css={titleArea}>
-              <Link href={INTERNAL_URL.MAIN} css={logoWrapper}>
-                <Image src={isThemeWhite ? logoBlue : logoWhite} alt="고초대졸_로고" />
-              </Link>
-              <div>
-                <FiSearch
-                  css={mobileIcon}
-                  onClick={() => {
-                    openSearchModal();
-                  }}
-                />
-                <FiBell css={mobileIcon} />
-                <FiUser
-                  css={mobileIcon}
+          <div css={titleArea}>
+            <Link href={INTERNAL_URL.MAIN} css={logoWrapper}>
+              <Image src={isThemeWhite ? logoBlue : logoWhite} alt="고초대졸_로고" />
+            </Link>
+            <div css={searchBarWrapper}>
+              <SearchBar
+                maxLength={0}
+                onClick={() => {
+                  openSearchModal();
+                }}
+                color={isThemeWhite ? "gray" : undefined}
+              />
+            </div>
+          </div>
+          <div css={navigationArea}>
+            <nav>
+              <ul css={navigationWrapper}>
+                <li css={menu(router.pathname.includes(INTERNAL_URL.JD))}>
+                  <Link href={INTERNAL_URL.JD}>채용공고</Link>
+                </li>
+                <li css={menu(router.pathname.includes(INTERNAL_URL.COMPANY))}>
+                  <Link href={INTERNAL_URL.COMPANY}>기업정보</Link>
+                </li>
+                {/* <li css={menu(router.pathname.includes(INTERNAL_URL.COMMUNITY))}> */}
+                {/*  <Link href={INTERNAL_URL.COMMUNITY}>커뮤니티</Link> */}
+                {/* </li> */}
+              </ul>
+            </nav>
+            <div css={etcWrapper}>
+              <a
+                href="https://blog.gochodaejol.com/?utm_source=gochodaejoldotcom&utm_medium=GNB"
+                target="_blank"
+                rel="noreferrer"
+                css={businessServiceButton}
+              >
+                블로그
+              </a>
+              <a
+                href="https://gocho.biz/?utm_source=gochodaejoldotcom&utm_medium=GNB"
+                target="_blank"
+                rel="noreferrer"
+                css={businessServiceButton}
+              >
+                기업서비스
+              </a>
+              {isSuccess ? (
+                <>
+                  <Alarm userId={userData.id} css={alarmIcon} />
+                  <DropDown
+                    customTitle={<Profile src={userData.image} size={40} altText={`${userData.nickname} 유저 로고`} />}
+                    menu={{
+                      width: 180,
+                      options: [
+                        {
+                          content: (
+                            <Link
+                              href={{
+                                pathname: INTERNAL_URL.MYPAGE,
+                                query: { type: "profile" },
+                              }}
+                            >
+                              프로필 관리
+                            </Link>
+                          ),
+                        },
+                        {
+                          content: (
+                            <Link
+                              href={{
+                                pathname: INTERNAL_URL.MYPAGE,
+                                query: { type: "account" },
+                              }}
+                            >
+                              계정 관리
+                            </Link>
+                          ),
+                        },
+                        {
+                          content: (
+                            <Link
+                              href={{
+                                pathname: INTERNAL_URL.MYPAGE,
+                                query: { type: "bookmark" },
+                              }}
+                            >
+                              북마크
+                            </Link>
+                          ),
+                        },
+                        {
+                          content: (
+                            <Link
+                              href={{
+                                pathname: INTERNAL_URL.MYPAGE,
+                                query: { type: "alarm" },
+                              }}
+                            >
+                              알림 설정
+                            </Link>
+                          ),
+                        },
+                      ],
+                      footer: { content: "로그아웃", onClick: doLogout },
+                    }}
+                    menuConfig={{
+                      closeAfterClickEvent: true,
+                    }}
+                  />
+                </>
+              ) : (
+                <button
+                  type="button"
+                  css={loginButton}
                   onClick={() => {
                     openLoginModal();
                   }}
-                />
-              </div>
+                >
+                  로그인/회원가입
+                </button>
+              )}
             </div>
-          ) : (
-            <>
-              <div css={titleArea}>
-                <Link href={INTERNAL_URL.MAIN} css={logoWrapper}>
-                  <Image src={isThemeWhite ? logoBlue : logoWhite} alt="고초대졸_로고" />
-                </Link>
-                <div css={searchBarWrapper}>
-                  <SearchBar
-                    maxLength={0}
-                    onClick={() => {
-                      openSearchModal();
-                    }}
-                    color={isThemeWhite ? "gray" : undefined}
-                  />
-                </div>
-              </div>
-              <div css={navigationArea}>
-                <nav>
-                  <ul css={navigationWrapper}>
-                    <li css={menu(router.pathname.includes(INTERNAL_URL.JD))}>
-                      <Link href={INTERNAL_URL.JD}>채용공고</Link>
-                    </li>
-                    <li css={menu(router.pathname.includes(INTERNAL_URL.COMPANY))}>
-                      <Link href={INTERNAL_URL.COMPANY}>기업정보</Link>
-                    </li>
-                    {/* <li css={menu(router.pathname.includes(INTERNAL_URL.COMMUNITY))}> */}
-                    {/*  <Link href={INTERNAL_URL.COMMUNITY}>커뮤니티</Link> */}
-                    {/* </li> */}
-                  </ul>
-                </nav>
-                <div css={etcWrapper}>
-                  <a
-                    href="https://blog.gochodaejol.com/?utm_source=gochodaejoldotcom&utm_medium=GNB"
-                    target="_blank"
-                    rel="noreferrer"
-                    css={businessServiceButton}
-                  >
-                    블로그
-                  </a>
-                  <a
-                    href="https://gocho.biz/?utm_source=gochodaejoldotcom&utm_medium=GNB"
-                    target="_blank"
-                    rel="noreferrer"
-                    css={businessServiceButton}
-                  >
-                    기업서비스
-                  </a>
-                  {isSuccess ? (
-                    <>
-                      <Alarm userId={userData.id} css={alarmIcon} />
-                      <DropDown
-                        customTitle={
-                          <Profile src={userData.image} size={40} altText={`${userData.nickname} 유저 로고`} />
-                        }
-                        menu={{
-                          width: 180,
-                          options: [
-                            {
-                              content: (
-                                <Link
-                                  href={{
-                                    pathname: INTERNAL_URL.MYPAGE,
-                                    query: { type: "profile" },
-                                  }}
-                                >
-                                  프로필 관리
-                                </Link>
-                              ),
-                            },
-                            {
-                              content: (
-                                <Link
-                                  href={{
-                                    pathname: INTERNAL_URL.MYPAGE,
-                                    query: { type: "account" },
-                                  }}
-                                >
-                                  계정 관리
-                                </Link>
-                              ),
-                            },
-                            {
-                              content: (
-                                <Link
-                                  href={{
-                                    pathname: INTERNAL_URL.MYPAGE,
-                                    query: { type: "bookmark" },
-                                  }}
-                                >
-                                  북마크
-                                </Link>
-                              ),
-                            },
-                            {
-                              content: (
-                                <Link
-                                  href={{
-                                    pathname: INTERNAL_URL.MYPAGE,
-                                    query: { type: "alarm" },
-                                  }}
-                                >
-                                  알림 설정
-                                </Link>
-                              ),
-                            },
-                          ],
-                          footer: { content: "로그아웃", onClick: doLogout },
-                        }}
-                        menuConfig={{
-                          closeAfterClickEvent: true,
-                        }}
-                      />
-                    </>
-                  ) : (
-                    <button
-                      type="button"
-                      css={loginButton}
-                      onClick={() => {
-                        openLoginModal();
-                      }}
-                    >
-                      로그인/회원가입
-                    </button>
-                  )}
-                </div>
-              </div>
-            </>
-          )}
+          </div>
         </Layout>
       </header>
       {loginModal && (
