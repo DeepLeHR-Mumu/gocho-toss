932a9db035c81160ee416fbeb5adebd2b30d5439
zac <yeongi@deeplehr.com>
2023-10-06 17:13:27 +0900

    [Fix] rename

--- apps/new_gocho/src/constants/queryKeyFactory/resume/resumeHighschoolKeyObj.ts
+++ apps/new_gocho/src/constants/queryKeyFactory/resume/resumeHighschoolKeyObj.ts
@@ -1,5 +1,6 @@
 export const resumeHighSchoolKeyObj = {
   all: [{ data: "resumeHighSchool" }] as const,
+
   highSchool: (resumeId: number, highSchoolId: number) =>
     [{ data: "resumeHighSchool", resumeId, highSchoolId }] as const,
 };
