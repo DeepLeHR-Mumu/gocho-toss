a62c1c1d21f12b6ebb6cf103af04bc7ec27d989c
Mumu <hsm9300@deeplehr.com>
2023-10-17 10:46:16 +0900

    [Fix] refactoring

--- apps/new_gocho/src/pages/company/component/index.ts
+++ apps/new_gocho/src/pages/company/component/index.ts
@@ -1,4 +1,3 @@
 export { CategoryItem } from "./CategoryItem";
 export { HeaderTitle } from "./HeaderTitle";
 export { RankCompanyCard } from "./RankCompanyCard";
-export { TodayIcon } from "./TodayIcon";
--- apps/new_gocho/src/pages/company/part/RankPart/style.ts
+++ apps/new_gocho/src/pages/company/part/RankPart/style.ts
@@ -10,6 +10,26 @@ export const cssObj = {
     gap: 1rem;
   `,
 
+  left: css`
+    position: absolute;
+    width: 1.25rem;
+    top: 0.5rem;
+    height: 1.25rem;
+    z-index: 3;
+  `,
+
+  today: css`
+    position: absolute;
+    left: 0.88rem;
+    width: 5rem;
+    height: 2.3rem;
+  `,
+
+  iconWrap: css`
+    position: relative;
+    display: flex;
+  `,
+
   companyList: css`
     margin-top: 3rem;
   `,
