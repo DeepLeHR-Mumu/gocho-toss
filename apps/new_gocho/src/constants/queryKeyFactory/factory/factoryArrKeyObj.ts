58e0622b971081bed09013cadafdd732bc2c359c
Mumu <hsm9300@deeplehr.com>
2023-10-17 15:36:37 +0900

    [Fix] GTM 설치, 사용하지 않는 queryKeyFactory 삭제

--- apps/new_gocho/src/constants/queryKeyFactory/factory/factoryArrKeyObj.ts
+++
@@ -1,8 +0,0 @@
-export interface FactoryArrRequestObjDef {
-  companyId: number;
-}
-
-export const factoryArrKeyObj = {
-  all: [{ data: "factoryArr" }] as const,
-  arr: (requestObj: FactoryArrRequestObjDef) => [{ data: "factoryArr", requestObj }] as const,
-};
--- apps/new_gocho/src/constants/queryKeyFactory/tip/detailKeyObj.ts
+++
@@ -1,8 +0,0 @@
-export interface TipRequestObjDef {
-  id: number;
-}
-
-export const tipDetailKeyObj = {
-  all: [{ data: "tipDetail" }] as const,
-  detail: (requestObj: TipRequestObjDef) => [{ data: "tipDetail", requestObj }] as const,
-};
