0b62c164a68c3e8a5bab514dd41775ed8664557a
wooyi <wychoi@deeplehr.com>
2023-10-27 15:34:59 +0900

    [Fix] Checkbox 롤백

--- shared/ui/deeple-ds/Checkbox/style.ts
+++ shared/ui/deeple-ds/Checkbox/style.ts
@@ -3,20 +3,40 @@ import { COLOR } from "shared-style/color";
 
 export const cssObj = {
   checkbox: css`
-    display: none;
-  `,
+    width: 1.125rem;
+    height: 1.125rem;
+    border: 1.5px solid ${COLOR.GRAY450};
+    border-radius: 0.25rem;
+    margin: 0;
 
-  check: (isChecked: boolean) => css`
-    cursor: pointer;
-    width: 1rem;
-    height: 1rem;
-    margin-right: 0.25rem;
-    background-color: ${isChecked ? `${COLOR.BLUE300}` : `${COLOR.WHITE}`};
-    border: 1px solid ${isChecked ? `${COLOR.BLUE300}` : `${COLOR.GRAY450}`};
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 3px;
-    color: ${COLOR.WHITE};
+    &:disabled {
+      border-color: ${COLOR.GRAY200};
+      cursor: not-allowed;
+    }
+
+    &[type="checkbox"]::after {
+      border: solid ${COLOR.WHITE};
+      border-width: 0 1px 1px 0;
+      content: "";
+      display: none;
+      height: 8px;
+      position: relative;
+      top: -2px;
+      transform: rotate(45deg);
+      width: 4px;
+    }
+
+    &[type="checkbox"]:checked {
+      background: ${COLOR.BLUE300};
+      border-color: ${COLOR.BLUE300};
+    }
+
+    &[type="checkbox"]:checked:disabled {
+      background: ${COLOR.GRAY200};
+    }
+
+    &[type="checkbox"]:checked::after {
+      display: block;
+    }
   `,
 };
